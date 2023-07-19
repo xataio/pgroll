f9e141f07a597327bc8e552b4916cdc340aac00f
Andrew Farries <andrew.farries@xata.io>
2023-07-19 08:39:34 +0100

    Add test for trigger deletion on rollback
    Ensure that the trigger function and trigger are removed when an add
    column operation (that specifies `up` SQL) is removed.

--- pkg/migrations/op_add_column.go
+++ pkg/migrations/op_add_column.go
@@ -61,7 +61,7 @@ func (o *OpAddColumn) Rollback(ctx context.Context, conn *sql.DB) error {
 	}
 
 	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-		pq.QuoteIdentifier(triggerFunctionName(o))))
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column.Name))))
 	return err
 }
 
@@ -97,8 +97,6 @@ func addColumn(ctx context.Context, conn *sql.DB, o OpAddColumn, t *schema.Table
 }
 
 func createTrigger(ctx context.Context, conn *sql.DB, o *OpAddColumn, schemaName, stateSchema string, s *schema.Schema) error {
-	triggerName := triggerFunctionName
-
 	// Generate the SQL declarations for the trigger function
 	// This results in declarations like:
 	//   col1 table.col1%TYPE := NEW.col1;
@@ -135,7 +133,7 @@ func createTrigger(ctx context.Context, conn *sql.DB, o *OpAddColumn, schemaName
 
       RETURN NEW;
     END; $$`,
-		pq.QuoteIdentifier(triggerFunctionName(o)),
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column.Name)),
 		pq.QuoteIdentifier(TemporaryName(o.Column.Name)),
 		*o.Up,
 		sqlDeclarations(s),
@@ -152,9 +150,9 @@ func createTrigger(ctx context.Context, conn *sql.DB, o *OpAddColumn, schemaName
     ON %[2]s
     FOR EACH ROW
     EXECUTE PROCEDURE %[3]s();`,
-		pq.QuoteIdentifier(triggerName(o)),
+		pq.QuoteIdentifier(TriggerName(o.Table, o.Column.Name)),
 		pq.QuoteIdentifier(o.Table),
-		pq.QuoteIdentifier(triggerFunctionName(o)))
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column.Name)))
 
 	_, err = conn.ExecContext(ctx, trigger)
 	if err != nil {
@@ -164,6 +162,10 @@ func createTrigger(ctx context.Context, conn *sql.DB, o *OpAddColumn, schemaName
 	return nil
 }
 
-func triggerFunctionName(o *OpAddColumn) string {
-	return "_pgroll_add_column_" + o.Table + "_" + o.Column.Name
+func TriggerFunctionName(tableName, columnName string) string {
+	return "_pgroll_add_column_" + tableName + "_" + columnName
+}
+
+func TriggerName(tableName, columnName string) string {
+	return TriggerFunctionName(tableName, columnName)
 }
