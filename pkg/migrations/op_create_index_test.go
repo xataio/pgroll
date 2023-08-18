b424081f994a669e1b2669fb2cde476f42137a98
Andrew Farries <andrew.farries@xata.io>
2023-08-18 07:58:03 +0100

    Make index name mandatory on create index op

--- pkg/migrations/op_create_index_test.go
+++ pkg/migrations/op_create_index_test.go
@@ -37,6 +37,7 @@ func TestCreateIndex(t *testing.T) {
 				Name: "02_create_index",
 				Operations: migrations.Operations{
 					&migrations.OpCreateIndex{
+						Name:    "idx_users_name",
 						Table:   "users",
 						Columns: []string{"name"},
 					},
@@ -45,68 +46,11 @@ func TestCreateIndex(t *testing.T) {
 		},
 		afterStart: func(t *testing.T, db *sql.DB) {
 			// The index has been created on the underlying table.
-			idxName := migrations.GenerateIndexName("users", []string{"name"})
-			IndexMustExist(t, db, "public", "users", idxName)
+			IndexMustExist(t, db, "public", "users", "idx_users_name")
 		},
 		afterRollback: func(t *testing.T, db *sql.DB) {
 			// The index has been dropped from the the underlying table.
-			idxName := migrations.GenerateIndexName("users", []string{"name"})
-			IndexMustNotExist(t, db, "public", "users", idxName)
-		},
-		afterComplete: func(t *testing.T, db *sql.DB) {
-			// Complete is a no-op.
-		},
-	}})
-}
-
-func TestCreateIndexWithUserSuppliedName(t *testing.T) {
-	t.Parallel()
-
-	ptr := func(s string) *string { return &s }
-
-	indexName := "idx_name"
-
-	ExecuteTests(t, TestCases{{
-		name: "create index with user supplied name",
-		migrations: []migrations.Migration{
-			{
-				Name: "01_add_table",
-				Operations: migrations.Operations{
-					&migrations.OpCreateTable{
-						Name: "users",
-						Columns: []migrations.Column{
-							{
-								Name:       "id",
-								Type:       "serial",
-								PrimaryKey: true,
-							},
-							{
-								Name:     "name",
-								Type:     "varchar(255)",
-								Nullable: false,
-							},
-						},
-					},
-				},
-			},
-			{
-				Name: "02_create_index",
-				Operations: migrations.Operations{
-					&migrations.OpCreateIndex{
-						Name:    ptr(indexName),
-						Table:   "users",
-						Columns: []string{"name"},
-					},
-				},
-			},
-		},
-		afterStart: func(t *testing.T, db *sql.DB) {
-			// The index has been created on the underlying table.
-			IndexMustExist(t, db, "public", "users", indexName)
-		},
-		afterRollback: func(t *testing.T, db *sql.DB) {
-			// The index has been dropped from the the underlying table.
-			IndexMustNotExist(t, db, "public", "users", indexName)
+			IndexMustNotExist(t, db, "public", "users", "idx_users_name")
 		},
 		afterComplete: func(t *testing.T, db *sql.DB) {
 			// Complete is a no-op.
@@ -149,6 +93,7 @@ func TestCreateIndexOnMultipleColumns(t *testing.T) {
 				Name: "02_create_index",
 				Operations: migrations.Operations{
 					&migrations.OpCreateIndex{
+						Name:    "idx_users_name_email",
 						Table:   "users",
 						Columns: []string{"name", "email"},
 					},
@@ -157,13 +102,11 @@ func TestCreateIndexOnMultipleColumns(t *testing.T) {
 		},
 		afterStart: func(t *testing.T, db *sql.DB) {
 			// The index has been created on the underlying table.
-			idxName := migrations.GenerateIndexName("users", []string{"name", "email"})
-			IndexMustExist(t, db, "public", "users", idxName)
+			IndexMustExist(t, db, "public", "users", "idx_users_name_email")
 		},
 		afterRollback: func(t *testing.T, db *sql.DB) {
 			// The index has been dropped from the the underlying table.
-			idxName := migrations.GenerateIndexName("users", []string{"name", "email"})
-			IndexMustNotExist(t, db, "public", "users", idxName)
+			IndexMustNotExist(t, db, "public", "users", "idx_users_name_email")
 		},
 		afterComplete: func(t *testing.T, db *sql.DB) {
 			// Complete is a no-op.
