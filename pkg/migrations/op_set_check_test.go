a105edd05ec023ff02f2b46e0d2b08f3958ca5a7
Andrew Farries <andyrb@gmail.com>
2023-09-11 06:17:28 +0100

    Implement adding check constraints to existing columns (#83)
    Add support for an operation to add a `CHECK` constraint to an existing
    column. The new operation looks like this:
    
    ```json
    {
      "name": "22_add_check_constraint",
      "operations": [
        {
          "set_check_constraint": {
            "table": "posts",
            "column": "title",
            "check": "length(title) > 3",
            "up": "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
            "down": "title"
          }
        }
      ]
    }
    ```
    
    This migrations adds a `CHECK (length(title) > 3)` constraint to the
    `title` column on the `posts` table. Pre-existing values in the old
    schema are rewritten to meet the constraint using the `up` SQL.
    
    The implementation is similar to the **set not null**, **change column
    type** and **set foreign key** operations.
    
    * On `Start`:
    * The column is duplicated and a `NOT VALID` `CHECK` constraint is added
    to the new column.
    * Values from the old column are backfilled into the new column using
    `up` SQL.
    * Triggers are created to copy values from old -> new with `up` SQL and
    from new->old using `down` SQL.
    * On `Complete`
      * The `CHECK` constraint is validated 
    * The old column is dropped and the new column renamed to the name of
    the old column.
    * Postgres ensures that the `CHECK` constraint is also updated to apply
    to the new column.
      * Triggers and trigger functions are removed.
    * On `Rollback`
      * The new column is removed
      * Triggers and trigger functions are removed.
    
    As with other operations involving `up` and `down` SQL, it is the user's
    responsibility to ensure that values from the old schema that don't meet
    the new `CHECK` constraint are correctly rewritten to meet the
    constraint with `up` SQL. If the `up` SQL fails to produce a value that
    meets the constraint, the migration will fail either at start (for
    existing values in the old schema) or at runtime (for values written to
    the old schema during the migration period).

---
+++ pkg/migrations/op_set_check_test.go
@@ -0,0 +1,253 @@
+package migrations_test
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/xataio/pg-roll/pkg/migrations"
+)
+
+func TestSetCheckConstraint(t *testing.T) {
+	t.Parallel()
+
+	ExecuteTests(t, TestCases{{
+		name: "add check constraint",
+		migrations: []migrations.Migration{
+			{
+				Name: "01_add_table",
+				Operations: migrations.Operations{
+					&migrations.OpCreateTable{
+						Name: "posts",
+						Columns: []migrations.Column{
+							{
+								Name:       "id",
+								Type:       "serial",
+								PrimaryKey: true,
+							},
+							{
+								Name: "title",
+								Type: "text",
+							},
+						},
+					},
+				},
+			},
+			{
+				Name: "02_add_check_constraint",
+				Operations: migrations.Operations{
+					&migrations.OpSetCheckConstraint{
+						Table:  "posts",
+						Column: "title",
+						Check:  "length(title) > 3",
+						Up:     "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+						Down:   "title",
+					},
+				},
+			},
+		},
+		afterStart: func(t *testing.T, db *sql.DB) {
+			// The new (temporary) `title` column should exist on the underlying table.
+			ColumnMustExist(t, db, "public", "posts", migrations.TemporaryName("title"))
+
+			// Inserting a row that meets the check constraint into the old view works.
+			MustInsert(t, db, "public", "01_add_table", "posts", map[string]string{
+				"title": "post by alice",
+			})
+
+			// Inserting a row that does not meet the check constraint into the old view also works.
+			MustInsert(t, db, "public", "01_add_table", "posts", map[string]string{
+				"title": "b",
+			})
+
+			// Both rows have been backfilled into the new view; the short title has
+			// been rewritten using `up` SQL to meet the length constraint.
+			rows := MustSelect(t, db, "public", "02_add_check_constraint", "posts")
+			assert.Equal(t, []map[string]any{
+				{"id": 1, "title": "post by alice"},
+				{"id": 2, "title": "---b"},
+			}, rows)
+
+			// Inserting a row that meets the check constraint into the new view works.
+			MustInsert(t, db, "public", "02_add_check_constraint", "posts", map[string]string{
+				"title": "post by carl",
+			})
+
+			// Inserting a row that does not meet the check constraint into the new view fails.
+			MustNotInsert(t, db, "public", "02_add_check_constraint", "posts", map[string]string{
+				"title": "d",
+			})
+
+			// The row that was inserted into the new view has been backfilled into the old view.
+			rows = MustSelect(t, db, "public", "01_add_table", "posts")
+			assert.Equal(t, []map[string]any{
+				{"id": 1, "title": "post by alice"},
+				{"id": 2, "title": "b"},
+				{"id": 3, "title": "post by carl"},
+			}, rows)
+		},
+		afterRollback: func(t *testing.T, db *sql.DB) {
+			// The new (temporary) `title` column should not exist on the underlying table.
+			ColumnMustNotExist(t, db, "public", "posts", migrations.TemporaryName("title"))
+
+			// The up function no longer exists.
+			FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", "title"))
+			// The down function no longer exists.
+			FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", migrations.TemporaryName("title")))
+
+			// The up trigger no longer exists.
+			TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", "title"))
+			// The down trigger no longer exists.
+			TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", migrations.TemporaryName("title")))
+		},
+		afterComplete: func(t *testing.T, db *sql.DB) {
+			// Inserting a row that meets the check constraint into the new view works.
+			MustInsert(t, db, "public", "02_add_check_constraint", "posts", map[string]string{
+				"title": "post by dana",
+			})
+
+			// Inserting a row that does not meet the check constraint into the new view fails.
+			MustNotInsert(t, db, "public", "02_add_check_constraint", "posts", map[string]string{
+				"title": "e",
+			})
+
+			// The data in the new `posts` view is as expected.
+			rows := MustSelect(t, db, "public", "02_add_check_constraint", "posts")
+			assert.Equal(t, []map[string]any{
+				{"id": 1, "title": "post by alice"},
+				{"id": 2, "title": "---b"},
+				{"id": 3, "title": "post by carl"},
+				{"id": 5, "title": "post by dana"},
+			}, rows)
+
+			// The up function no longer exists.
+			FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", "title"))
+			// The down function no longer exists.
+			FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", migrations.TemporaryName("title")))
+
+			// The up trigger no longer exists.
+			TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", "title"))
+			// The down trigger no longer exists.
+			TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", migrations.TemporaryName("title")))
+		},
+	}})
+}
+
+func TestSetCheckConstraintValidation(t *testing.T) {
+	t.Parallel()
+
+	createTableMigration := migrations.Migration{
+		Name: "01_add_table",
+		Operations: migrations.Operations{
+			&migrations.OpCreateTable{
+				Name: "posts",
+				Columns: []migrations.Column{
+					{
+						Name:       "id",
+						Type:       "serial",
+						PrimaryKey: true,
+					},
+					{
+						Name: "title",
+						Type: "text",
+					},
+				},
+			},
+		},
+	}
+
+	ExecuteTests(t, TestCases{
+		{
+			name: "table must exist",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				{
+					Name: "02_add_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpSetCheckConstraint{
+							Table:  "doesntexist",
+							Column: "title",
+							Check:  "length(title) > 3",
+							Up:     "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+							Down:   "title",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.TableDoesNotExistError{Name: "doesntexist"},
+		},
+		{
+			name: "column must exist",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				{
+					Name: "02_add_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpSetCheckConstraint{
+							Table:  "posts",
+							Column: "doesntexist",
+							Check:  "length(title) > 3",
+							Up:     "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+							Down:   "title",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.ColumnDoesNotExistError{Table: "posts", Name: "doesntexist"},
+		},
+		{
+			name: "check SQL is mandatory",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				{
+					Name: "02_add_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpSetCheckConstraint{
+							Table:  "posts",
+							Column: "title",
+							Up:     "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+							Down:   "title",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.FieldRequiredError{Name: "check"},
+		},
+		{
+			name: "up SQL is mandatory",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				{
+					Name: "02_add_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpSetCheckConstraint{
+							Table:  "posts",
+							Column: "title",
+							Check:  "length(title) > 3",
+							Down:   "title",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.FieldRequiredError{Name: "up"},
+		},
+		{
+			name: "down SQL is mandatory",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				{
+					Name: "02_add_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpSetCheckConstraint{
+							Table:  "posts",
+							Column: "title",
+							Check:  "length(title) > 3",
+							Up:     "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.FieldRequiredError{Name: "down"},
+		},
+	})
+}
