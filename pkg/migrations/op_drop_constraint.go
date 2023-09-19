31402f9f7a4b5ccb6c51e75e0f4099986a32db98
Andrew Farries <andyrb@gmail.com>
2023-09-19 05:32:53 +0100

    Implement the `drop_constraint` operation (#103)
    Implement the `drop_constraint` operation for dropping constraints
    defined on single columns.
    
    An example of the operation looks like:
    
    ```json
    {
      "name": "23_drop_check_constraint",
      "operations": [
        {
          "drop_constraint": {
            "table": "posts",
            "column": "title",
            "name": "title_length",
            "up": "title",
            "down": "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)"
          }
        }
      ]
    }
    ```
    
    for dropping a `CHECK` constraint. And like this for dropping a `FOREIGN
    KEY` constraint:
    
    
    ```json
    {
      "name": "24_drop_foreign_key_constraint",
      "operations": [
        {
          "drop_constraint": {
            "table": "posts",
            "column": "user_id",
            "name": "fk_users_id",
            "up": "user_id",
            "down": "(SELECT CASE WHEN EXISTS (SELECT 1 FROM users WHERE users.id = user_id) THEN user_id ELSE NULL END)"
          }
        }
      ]
    }
    ```
    
    The operation works very similarly to the inverse operation of adding
    `CHECK` and `FOREIGN KEY` constraints to a single column.
    
    * On `Start`:
    * a new column without the constraint is added to the underlying table.
    * triggers are created using the `up` and `down` SQL. The `down` SQL
    needs to ensure that rows inserted into the new view that don't meet the
    constraint are converted into rows that do meet the constraint.
    * On `Complete`
    * Triggers are removed, the old column is deleted and the new column is
    renamed.
    * On `Rollback`
      * The new column and the triggers are removed.  
    
    ## Improvements
    * The `drop_constraint` operation requires that the column on which the
    constraint is defined is named in the migration `json` file. If
    `pg-roll`'s internal schema representation knew about the constraints
    defined on a table it would be possible to delete constraints by
    constraint name only; the schema representation would know on which
    column the constraint was defined.
    * `pg-roll` currently only allows for creating `CHECK` and `FOREIGN KEY`
    constraints on single columns; this limitation also applies to the
    `drop_constraint` operation.

---
+++ pkg/migrations/op_drop_constraint.go
@@ -0,0 +1,160 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/lib/pq"
+	"github.com/xataio/pg-roll/pkg/schema"
+)
+
+type OpDropConstraint struct {
+	Table  string `json:"table"`
+	Column string `json:"column"`
+	Name   string `json:"name"`
+	Up     string `json:"up"`
+	Down   string `json:"down"`
+}
+
+var _ Operation = (*OpDropConstraint)(nil)
+
+func (o *OpDropConstraint) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema) error {
+	table := s.GetTable(o.Table)
+	column := table.GetColumn(o.Column)
+
+	// Create a copy of the column on the underlying table.
+	if err := duplicateColumn(ctx, conn, table, *column); err != nil {
+		return fmt.Errorf("failed to duplicate column: %w", err)
+	}
+
+	// Add a trigger to copy values from the old column to the new, rewriting values using the `up` SQL.
+	err := createTrigger(ctx, conn, triggerConfig{
+		Name:           TriggerName(o.Table, o.Column),
+		Direction:      TriggerDirectionUp,
+		Columns:        table.Columns,
+		SchemaName:     s.Name,
+		TableName:      o.Table,
+		PhysicalColumn: TemporaryName(o.Column),
+		StateSchema:    stateSchema,
+		SQL:            o.Up,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create up trigger: %w", err)
+	}
+
+	// Backfill the new column with values from the old column.
+	if err := backFill(ctx, conn, o.Table, TemporaryName(o.Column)); err != nil {
+		return fmt.Errorf("failed to backfill column: %w", err)
+	}
+
+	// Add the new column to the internal schema representation. This is done
+	// here, before creation of the down trigger, so that the trigger can declare
+	// a variable for the new column.
+	table.AddColumn(o.Column, schema.Column{
+		Name: TemporaryName(o.Column),
+	})
+
+	// Add a trigger to copy values from the new column to the old, rewriting values using the `down` SQL.
+	err = createTrigger(ctx, conn, triggerConfig{
+		Name:           TriggerName(o.Table, TemporaryName(o.Column)),
+		Direction:      TriggerDirectionDown,
+		Columns:        table.Columns,
+		SchemaName:     s.Name,
+		TableName:      o.Table,
+		PhysicalColumn: o.Column,
+		StateSchema:    stateSchema,
+		SQL:            o.Down,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create down trigger: %w", err)
+	}
+	return nil
+}
+
+func (o *OpDropConstraint) Complete(ctx context.Context, conn *sql.DB) error {
+	// Remove the up function and trigger
+	_, err := conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column))))
+	if err != nil {
+		return err
+	}
+
+	// Remove the down function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column)))))
+	if err != nil {
+		return err
+	}
+
+	// Drop the old column
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE IF EXISTS %s DROP COLUMN IF EXISTS %s",
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(o.Column)))
+	if err != nil {
+		return err
+	}
+
+	// Rename the new column to the old column name
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE IF EXISTS %s RENAME COLUMN %s TO %s",
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(TemporaryName(o.Column)),
+		pq.QuoteIdentifier(o.Column)))
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (o *OpDropConstraint) Rollback(ctx context.Context, conn *sql.DB) error {
+	// Drop the new column
+	_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE %s DROP COLUMN IF EXISTS %s",
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(TemporaryName(o.Column)),
+	))
+	if err != nil {
+		return err
+	}
+
+	// Remove the up function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column)),
+	))
+	if err != nil {
+		return err
+	}
+
+	// Remove the down function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column))),
+	))
+
+	return err
+}
+
+func (o *OpDropConstraint) Validate(ctx context.Context, s *schema.Schema) error {
+	table := s.GetTable(o.Table)
+	if table == nil {
+		return TableDoesNotExistError{Name: o.Table}
+	}
+
+	column := table.GetColumn(o.Column)
+	if column == nil {
+		return ColumnDoesNotExistError{Table: o.Table, Name: o.Column}
+	}
+
+	if o.Name == "" {
+		return FieldRequiredError{Name: "name"}
+	}
+
+	if o.Up == "" {
+		return FieldRequiredError{Name: "up"}
+	}
+
+	if o.Down == "" {
+		return FieldRequiredError{Name: "down"}
+	}
+
+	return nil
+}
---
+++ pkg/migrations/op_drop_constraint_test.go
@@ -0,0 +1,446 @@
+package migrations_test
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/xataio/pg-roll/pkg/migrations"
+)
+
+func TestDropConstraint(t *testing.T) {
+	t.Parallel()
+
+	ExecuteTests(t, TestCases{
+		{
+			name: "drop check constraint",
+			migrations: []migrations.Migration{
+				{
+					Name: "01_add_table",
+					Operations: migrations.Operations{
+						&migrations.OpCreateTable{
+							Name: "posts",
+							Columns: []migrations.Column{
+								{
+									Name:       "id",
+									Type:       "serial",
+									PrimaryKey: true,
+								},
+								{
+									Name: "title",
+									Type: "text",
+								},
+							},
+						},
+					},
+				},
+				{
+					Name: "02_add_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpAlterColumn{
+							Table:          "posts",
+							Column:         "title",
+							ConstraintName: "check_title_length",
+							Check:          "length(title) > 3",
+							Up:             "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+							Down:           "title",
+						},
+					},
+				},
+				{
+					Name: "03_drop_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpDropConstraint{
+							Table:  "posts",
+							Column: "title",
+							Name:   "check_title_length",
+							Up:     "title",
+							Down:   "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+						},
+					},
+				},
+			},
+			afterStart: func(t *testing.T, db *sql.DB) {
+				// The new (temporary) `title` column should exist on the underlying table.
+				ColumnMustExist(t, db, "public", "posts", migrations.TemporaryName("title"))
+
+				// Inserting a row that meets the check constraint into the old view works.
+				MustInsert(t, db, "public", "02_add_check_constraint", "posts", map[string]string{
+					"title": "post by alice",
+				})
+
+				// Inserting a row that does not meet the check constraint into the old view fails.
+				MustNotInsert(t, db, "public", "02_add_check_constraint", "posts", map[string]string{
+					"title": "b",
+				})
+
+				// The inserted row has been backfilled into the new view.
+				rows := MustSelect(t, db, "public", "03_drop_check_constraint", "posts")
+				assert.Equal(t, []map[string]any{
+					{"id": 1, "title": "post by alice"},
+				}, rows)
+
+				// Inserting a row that meets the check constraint into the new view works.
+				MustInsert(t, db, "public", "03_drop_check_constraint", "posts", map[string]string{
+					"title": "post by carl",
+				})
+
+				// Inserting a row that does not meet the check constraint into the new view also works.
+				MustInsert(t, db, "public", "03_drop_check_constraint", "posts", map[string]string{
+					"title": "d",
+				})
+
+				// Both rows that were inserted into the new view have been backfilled
+				// into the old view. The short `title` value has been rewritten to meet the
+				// check constraint present on the old view.
+				rows = MustSelect(t, db, "public", "02_add_check_constraint", "posts")
+				assert.Equal(t, []map[string]any{
+					{"id": 1, "title": "post by alice"},
+					{"id": 3, "title": "post by carl"},
+					{"id": 4, "title": "---d"},
+				}, rows)
+			},
+			afterRollback: func(t *testing.T, db *sql.DB) {
+				// The new (temporary) `title` column should not exist on the underlying table.
+				ColumnMustNotExist(t, db, "public", "posts", migrations.TemporaryName("title"))
+
+				// The up function no longer exists.
+				FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", "title"))
+				// The down function no longer exists.
+				FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", migrations.TemporaryName("title")))
+
+				// The up trigger no longer exists.
+				TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", "title"))
+				// The down trigger no longer exists.
+				TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", migrations.TemporaryName("title")))
+			},
+			afterComplete: func(t *testing.T, db *sql.DB) {
+				// Inserting a row that does not meet the check constraint into the new view works.
+				MustInsert(t, db, "public", "03_drop_check_constraint", "posts", map[string]string{
+					"title": "e",
+				})
+
+				// The data in the new `posts` view is as expected.
+				rows := MustSelect(t, db, "public", "03_drop_check_constraint", "posts")
+				assert.Equal(t, []map[string]any{
+					{"id": 1, "title": "post by alice"},
+					{"id": 3, "title": "post by carl"},
+					{"id": 4, "title": "---d"},
+					{"id": 5, "title": "e"},
+				}, rows)
+
+				// The up function no longer exists.
+				FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", "title"))
+				// The down function no longer exists.
+				FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", migrations.TemporaryName("title")))
+
+				// The up trigger no longer exists.
+				TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", "title"))
+				// The down trigger no longer exists.
+				TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", migrations.TemporaryName("title")))
+			},
+		},
+		{
+			name: "drop foreign key constraint",
+			migrations: []migrations.Migration{
+				{
+					Name: "01_add_tables",
+					Operations: migrations.Operations{
+						&migrations.OpCreateTable{
+							Name: "users",
+							Columns: []migrations.Column{
+								{
+									Name:       "id",
+									Type:       "serial",
+									PrimaryKey: true,
+								},
+								{
+									Name: "name",
+									Type: "text",
+								},
+							},
+						},
+						&migrations.OpCreateTable{
+							Name: "posts",
+							Columns: []migrations.Column{
+								{
+									Name:       "id",
+									Type:       "serial",
+									PrimaryKey: true,
+								},
+								{
+									Name: "title",
+									Type: "text",
+								},
+								{
+									Name: "user_id",
+									Type: "integer",
+								},
+							},
+						},
+					},
+				},
+				{
+					Name: "02_add_fk_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpAlterColumn{
+							Table:  "posts",
+							Column: "user_id",
+							References: &migrations.ForeignKeyReference{
+								Name:   "fk_users_id",
+								Table:  "users",
+								Column: "id",
+							},
+							Up:   "(SELECT CASE WHEN EXISTS (SELECT 1 FROM users WHERE users.id = user_id) THEN user_id ELSE NULL END)",
+							Down: "user_id",
+						},
+					},
+				},
+				{
+					Name: "03_drop_fk_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpDropConstraint{
+							Table:  "posts",
+							Column: "user_id",
+							Name:   "fk_users_id",
+							Up:     "user_id",
+							Down:   "(SELECT CASE WHEN EXISTS (SELECT 1 FROM users WHERE users.id = user_id) THEN user_id ELSE NULL END)",
+						},
+					},
+				},
+			},
+			afterStart: func(t *testing.T, db *sql.DB) {
+				// The new (temporary) `user_id` column should exist on the underlying table.
+				ColumnMustExist(t, db, "public", "posts", migrations.TemporaryName("user_id"))
+
+				// Inserting some data into the `users` table works.
+				MustInsert(t, db, "public", "03_drop_fk_constraint", "users", map[string]string{
+					"name": "alice",
+				})
+				MustInsert(t, db, "public", "03_drop_fk_constraint", "users", map[string]string{
+					"name": "bob",
+				})
+
+				// Inserting data into the new `posts` view with a valid user reference works.
+				MustInsert(t, db, "public", "03_drop_fk_constraint", "posts", map[string]string{
+					"title":   "post by alice",
+					"user_id": "1",
+				})
+
+				// Inserting data into the new `posts` view with an invalid user reference also works.
+				MustInsert(t, db, "public", "03_drop_fk_constraint", "posts", map[string]string{
+					"title":   "post by unknown user",
+					"user_id": "3",
+				})
+
+				// The inserted rows have been backfilled into the old view.
+				// The invalid user reference has been rewritten to NULL.
+				rows := MustSelect(t, db, "public", "02_add_fk_constraint", "posts")
+				assert.Equal(t, []map[string]any{
+					{"id": 1, "title": "post by alice", "user_id": 1},
+					{"id": 2, "title": "post by unknown user", "user_id": nil},
+				}, rows)
+
+				// Inserting data into the old `posts` view with a valid user reference works.
+				MustInsert(t, db, "public", "02_add_fk_constraint", "posts", map[string]string{
+					"title":   "post by bob",
+					"user_id": "2",
+				})
+
+				// Inserting data into the old `posts` view with an invalid user reference fails.
+				MustNotInsert(t, db, "public", "02_add_fk_constraint", "posts", map[string]string{
+					"title":   "post by unknown user",
+					"user_id": "3",
+				})
+
+				// The post that was inserted successfully has been backfilled into the new view.
+				rows = MustSelect(t, db, "public", "03_drop_fk_constraint", "posts")
+				assert.Equal(t, []map[string]any{
+					{"id": 1, "title": "post by alice", "user_id": 1},
+					{"id": 2, "title": "post by unknown user", "user_id": 3},
+					{"id": 3, "title": "post by bob", "user_id": 2},
+				}, rows)
+			},
+			afterRollback: func(t *testing.T, db *sql.DB) {
+				// The new (temporary) `user_id` column should not exist on the underlying table.
+				ColumnMustNotExist(t, db, "public", "posts", migrations.TemporaryName("user_id"))
+
+				// The up function no longer exists.
+				FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", "user_id"))
+				// The down function no longer exists.
+				FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", migrations.TemporaryName("user_id")))
+
+				// The up trigger no longer exists.
+				TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", "user_id"))
+				// The down trigger no longer exists.
+				TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", migrations.TemporaryName("user_id")))
+			},
+			afterComplete: func(t *testing.T, db *sql.DB) {
+				// The new (temporary) `user_id` column should not exist on the underlying table.
+				ColumnMustNotExist(t, db, "public", "posts", migrations.TemporaryName("user_id"))
+
+				// Inserting a row that does not meet the check constraint into the new view works.
+				MustInsert(t, db, "public", "03_drop_fk_constraint", "posts", map[string]string{
+					"title":   "another post by an unknown user",
+					"user_id": "4",
+				})
+
+				// The data in the new `posts` view is as expected.
+				rows := MustSelect(t, db, "public", "03_drop_fk_constraint", "posts")
+				assert.Equal(t, []map[string]any{
+					{"id": 1, "title": "post by alice", "user_id": 1},
+					{"id": 2, "title": "post by unknown user", "user_id": nil},
+					{"id": 3, "title": "post by bob", "user_id": 2},
+					{"id": 5, "title": "another post by an unknown user", "user_id": 4},
+				}, rows)
+
+				// The up function no longer exists.
+				FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", "user_id"))
+				// The down function no longer exists.
+				FunctionMustNotExist(t, db, "public", migrations.TriggerFunctionName("posts", migrations.TemporaryName("user_id")))
+
+				// The up trigger no longer exists.
+				TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", "user_id"))
+				// The down trigger no longer exists.
+				TriggerMustNotExist(t, db, "public", "posts", migrations.TriggerName("posts", migrations.TemporaryName("user_id")))
+			},
+		},
+	})
+}
+
+func TestDropConstraintValidation(t *testing.T) {
+	t.Parallel()
+
+	createTableMigration := migrations.Migration{
+		Name: "01_add_table",
+		Operations: migrations.Operations{
+			&migrations.OpCreateTable{
+				Name: "posts",
+				Columns: []migrations.Column{
+					{
+						Name:       "id",
+						Type:       "serial",
+						PrimaryKey: true,
+					},
+					{
+						Name: "title",
+						Type: "text",
+					},
+				},
+			},
+		},
+	}
+	addCheckMigration := migrations.Migration{
+		Name: "02_add_check_constraint",
+		Operations: migrations.Operations{
+			&migrations.OpAlterColumn{
+				Table:          "posts",
+				Column:         "title",
+				ConstraintName: "check_title_length",
+				Check:          "length(title) > 3",
+				Up:             "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+				Down:           "title",
+			},
+		},
+	}
+
+	ExecuteTests(t, TestCases{
+		{
+			name: "table must exist",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				addCheckMigration,
+				{
+					Name: "03_drop_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpDropConstraint{
+							Table:  "doesntexist",
+							Column: "title",
+							Name:   "check_title_length",
+							Up:     "title",
+							Down:   "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.TableDoesNotExistError{Name: "doesntexist"},
+		},
+		{
+			name: "column must exist",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				addCheckMigration,
+				{
+					Name: "03_drop_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpDropConstraint{
+							Table:  "posts",
+							Column: "doesntexist",
+							Name:   "check_title_length",
+							Up:     "title",
+							Down:   "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.ColumnDoesNotExistError{Table: "posts", Name: "doesntexist"},
+		},
+		{
+			name: "name is mandatory",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				addCheckMigration,
+				{
+					Name: "03_drop_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpDropConstraint{
+							Table:  "posts",
+							Column: "title",
+							Up:     "title",
+							Down:   "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.FieldRequiredError{Name: "name"},
+		},
+		{
+			name: "up SQL is mandatory",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				addCheckMigration,
+				{
+					Name: "03_drop_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpDropConstraint{
+							Table:  "posts",
+							Column: "title",
+							Name:   "check_title_length",
+							Down:   "(SELECT CASE WHEN length(title) <= 3 THEN LPAD(title, 4, '-') ELSE title END)",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.FieldRequiredError{Name: "up"},
+		},
+		{
+			name: "down SQL is mandatory",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				addCheckMigration,
+				{
+					Name: "03_drop_check_constraint",
+					Operations: migrations.Operations{
+						&migrations.OpDropConstraint{
+							Table:  "posts",
+							Column: "title",
+							Name:   "check_title_length",
+							Up:     "title",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.FieldRequiredError{Name: "down"},
+		},
+	})
+}
