b8b467cf32745f3c4de077750da6cc0855e891b5
Andrew Farries <andyrb@gmail.com>
2023-09-21 09:22:28 +0100

    Move the `set_unique` operation under `alter_column` (#116)
    Move the `set_unique` operation into the `alter_column` operation and
    restrict it to creating constraints on single columns.
    
    `set_unique` operations used to look like this:
    
    ```json
    {
      "name": "15_set_columns_unique",
      "operations": [
        {
          "set_unique": {
            "name": "reviews_username_product_unique",
            "table": "reviews",
            "columns": [
              "username",
              "product"
            ]
          }
        }
      ]
    }
    ```
    where the operation was called `set_unique` and multi-column constraints
    could be defined. The operation now looks like:
    
    
    ```json
    {
      "name": "15_set_column_unique",
      "operations": [
        {
          "alter_column": {
            "table": "reviews",
            "column": "review",
            "unique": {
              "name": "reviews_review_unique"
            }
          }
        }
      ]
    }
    ```
    where the operation is now `alter_column` and only a single column is
    covered by the constraint.
    
    Restricting the constraint to a single column makes it possible to have
    the operation duplicate the affected column and add up/down triggers
    when starting a migration as we do for other `alter_column` operations.

--- pkg/migrations/op_set_unique_test.go
+++ pkg/migrations/op_set_unique_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/xataio/pg-roll/pkg/migrations"
 )
 
-func TestSetColumnsUnique(t *testing.T) {
+func TestSetColumnUnique(t *testing.T) {
 	t.Parallel()
 
 	ExecuteTests(t, TestCases{{
@@ -46,48 +46,50 @@ func TestSetColumnsUnique(t *testing.T) {
 			{
 				Name: "02_set_unique",
 				Operations: migrations.Operations{
-					&migrations.OpSetUnique{
-						Name:    "reviews_username_product_unique",
-						Table:   "reviews",
-						Columns: []string{"username", "product"},
+					&migrations.OpAlterColumn{
+						Table:  "reviews",
+						Column: "review",
+						Unique: &migrations.UniqueConstraint{
+							Name: "reviews_review_unique",
+						},
 					},
 				},
 			},
 		},
 		afterStart: func(t *testing.T, db *sql.DB) {
 			// The unique index has been created on the underlying table.
-			IndexMustExist(t, db, "public", "reviews", "reviews_username_product_unique")
+			IndexMustExist(t, db, "public", "reviews", "reviews_review_unique")
 
 			// Inserting values into the old schema that violate uniqueness should fail.
 			MustInsert(t, db, "public", "01_add_table", "reviews", map[string]string{
 				"username": "alice", "product": "apple", "review": "good",
 			})
 			MustNotInsert(t, db, "public", "01_add_table", "reviews", map[string]string{
-				"username": "alice", "product": "apple", "review": "bad",
+				"username": "bob", "product": "banana", "review": "good",
 			})
 
 			// Inserting values into the new schema that violate uniqueness should fail.
 			MustInsert(t, db, "public", "02_set_unique", "reviews", map[string]string{
-				"username": "bob", "product": "orange", "review": "good",
+				"username": "carl", "product": "carrot", "review": "bad",
 			})
 			MustNotInsert(t, db, "public", "02_set_unique", "reviews", map[string]string{
-				"username": "bob", "product": "orange", "review": "bad",
+				"username": "dana", "product": "durian", "review": "bad",
 			})
 		},
 		afterRollback: func(t *testing.T, db *sql.DB) {
 			// The unique index has been dropped from the the underlying table.
-			IndexMustNotExist(t, db, "public", "reviews", "reviews_username_product_unique")
+			IndexMustNotExist(t, db, "public", "reviews", "reviews_review_unique")
 		},
 		afterComplete: func(t *testing.T, db *sql.DB) {
 			// The unique constraint has been created on the underlying table.
-			ConstraintMustExist(t, db, "public", "reviews", "reviews_username_product_unique")
+			ConstraintMustExist(t, db, "public", "reviews", "reviews_review_unique")
 
 			// Inserting values into the new schema that violate uniqueness should fail.
 			MustInsert(t, db, "public", "02_set_unique", "reviews", map[string]string{
-				"username": "carl", "product": "banana", "review": "good",
+				"username": "earl", "product": "elderberry", "review": "ok",
 			})
 			MustNotInsert(t, db, "public", "02_set_unique", "reviews", map[string]string{
-				"username": "carl", "product": "banana", "review": "bad",
+				"username": "flora", "product": "fig", "review": "ok",
 			})
 		},
 	}})
