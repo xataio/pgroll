61563db030df5f404f101a64315c648d735741b1
Andrew Farries <andrew.farries@xata.io>
2023-09-21 09:06:49 +0100

    Duplicate and add triggers when setting `UNIQUE`

--- pkg/migrations/op_set_unique.go
+++ pkg/migrations/op_set_unique.go
@@ -13,32 +13,136 @@ type OpSetUnique struct {
 	Name   string `json:"name"`
 	Table  string `json:"table"`
 	Column string `json:"column"`
+	Up     string `json:"up"`
+	Down   string `json:"down"`
 }
 
 var _ Operation = (*OpSetUnique)(nil)
 
 func (o *OpSetUnique) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema) error {
-	// create unique index concurrently
-	_, err := conn.ExecContext(ctx, fmt.Sprintf("CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS %s ON %s (%s)",
-		pq.QuoteIdentifier(o.Name),
-		pq.QuoteIdentifier(o.Table),
-		pq.QuoteIdentifier(o.Column)))
-	return err
+	table := s.GetTable(o.Table)
+	column := table.GetColumn(o.Column)
+
+	// create a copy of the column on the underlying table.
+	if err := duplicateColumn(ctx, conn, table, *column); err != nil {
+		return fmt.Errorf("failed to duplicate column: %w", err)
+	}
+
+	// Add a unique index to the new column
+	if err := o.addUniqueIndex(ctx, conn); err != nil {
+		return fmt.Errorf("failed to add unique index: %w", err)
+	}
+
+	// Add a trigger to copy values from the old column to the new, rewriting values using the `up` SQL.
+	err := createTrigger(ctx, conn, triggerConfig{
+		Name:           TriggerName(o.Table, o.Column),
+		Direction:      TriggerDirectionUp,
+		Columns:        table.Columns,
+		SchemaName:     s.Name,
+		TableName:      o.Table,
+		PhysicalColumn: TemporaryName(o.Column),
+		StateSchema:    stateSchema,
+		SQL:            o.Up,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create up trigger: %w", err)
+	}
+
+	// Backfill the new column with values from the old column.
+	if err := backFill(ctx, conn, o.Table, TemporaryName(o.Column)); err != nil {
+		return fmt.Errorf("failed to backfill column: %w", err)
+	}
+
+	// Add the new column to the internal schema representation. This is done
+	// here, before creation of the down trigger, so that the trigger can declare
+	// a variable for the new column.
+	table.AddColumn(o.Column, schema.Column{
+		Name: TemporaryName(o.Column),
+	})
+
+	// Add a trigger to copy values from the new column to the old, rewriting values using the `down` SQL.
+	err = createTrigger(ctx, conn, triggerConfig{
+		Name:           TriggerName(o.Table, TemporaryName(o.Column)),
+		Direction:      TriggerDirectionDown,
+		Columns:        table.Columns,
+		SchemaName:     s.Name,
+		TableName:      o.Table,
+		PhysicalColumn: o.Column,
+		StateSchema:    stateSchema,
+		SQL:            o.Down,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create down trigger: %w", err)
+	}
+
+	return nil
 }
 
 func (o *OpSetUnique) Complete(ctx context.Context, conn *sql.DB) error {
-	// create a unique constraint using the unique index
+	// Create a unique constraint using the unique index
 	_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE IF EXISTS %s ADD CONSTRAINT %s UNIQUE USING INDEX %s",
 		pq.QuoteIdentifier(o.Table),
 		pq.QuoteIdentifier(o.Name),
 		pq.QuoteIdentifier(o.Name)))
+	if err != nil {
+		return err
+	}
+
+	// Remove the up function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column)),
+	))
+	if err != nil {
+		return err
+	}
+
+	// Remove the down function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column))),
+	))
+	if err != nil {
+		return err
+	}
+
+	// Drop the old column
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE IF EXISTS %s DROP COLUMN IF EXISTS %s",
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(o.Column)))
+	if err != nil {
+		return err
+	}
+
+	// Rename the new column to the old column name
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE IF EXISTS %s RENAME COLUMN %s TO %s",
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(TemporaryName(o.Column)),
+		pq.QuoteIdentifier(o.Column)))
 
 	return err
 }
 
 func (o *OpSetUnique) Rollback(ctx context.Context, conn *sql.DB) error {
-	// drop the index concurrently
-	_, err := conn.ExecContext(ctx, fmt.Sprintf("DROP INDEX CONCURRENTLY IF EXISTS %s", o.Name))
+	// Drop the new column, taking the unique index on the column with it
+	_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE %s DROP COLUMN IF EXISTS %s",
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(TemporaryName(o.Column)),
+	))
+	if err != nil {
+		return err
+	}
+
+	// Remove the up function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column)),
+	))
+	if err != nil {
+		return err
+	}
+
+	// Remove the down function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column))),
+	))
 
 	return err
 }
@@ -58,4 +162,14 @@ func (o *OpSetUnique) Validate(ctx context.Context, s *schema.Schema) error {
 	}
 
 	return nil
+}
+
+func (o *OpSetUnique) addUniqueIndex(ctx context.Context, conn *sql.DB) error {
+	// create unique index concurrently
+	_, err := conn.ExecContext(ctx, fmt.Sprintf("CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS %s ON %s (%s)",
+		pq.QuoteIdentifier(o.Name),
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(TemporaryName(o.Column))))
+
+	return err
 }
