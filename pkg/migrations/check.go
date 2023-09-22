9517e3e47d9b50d97918a48f30f160810f08981e
Philip Krauss <philip@philipkrauss.at>
2023-09-22 10:48:30 +0200

    missing checkout

--- pkg/migrations/check.go
+++ pkg/migrations/check.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package migrations
 
 type CheckConstraint struct {
--- pkg/migrations/templates/trigger.go
+++ pkg/migrations/templates/trigger.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package templates
 
 const Trigger = `CREATE OR REPLACE TRIGGER {{ .Name | qi }}
