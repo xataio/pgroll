71d14ad51300fa61a0f72e3a751f0e20f216b16a
Philip Krauss <35487337+philkra@users.noreply.github.com>
2023-09-22 12:31:49 +0200

    License check in github actions (#123)
    This PR adds a check to ensure a license declaration is present in the
    file head of every `.go` file.

--- pkg/migrations/fk_reference.go
+++ pkg/migrations/fk_reference.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package migrations
 
 import "github.com/xataio/pg-roll/pkg/schema"
--- pkg/migrations/templates/function.go
+++ pkg/migrations/templates/function.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package templates
 
 const Function = `CREATE OR REPLACE FUNCTION {{ .Name | qi }}()
