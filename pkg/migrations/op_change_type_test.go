845ead316cd08603dc9c53815763a5bfa3cc6e11
Andrew Farries <andrew.farries@xata.io>
2024-01-18 15:34:23 +0000

    Update test expectations
    Update test expectations now that FK constraints on duplicated columns
    have a special prefix.

--- pkg/migrations/op_change_type_test.go
+++ pkg/migrations/op_change_type_test.go
@@ -213,7 +213,7 @@ func TestChangeColumnType(t *testing.T) {
 			},
 			afterStart: func(t *testing.T, db *sql.DB) {
 				// A temporary FK constraint has been created on the temporary column
-				ValidatedForeignKeyMustExist(t, db, "public", "employees", migrations.TemporaryName("fk_employee_department"))
+				ValidatedForeignKeyMustExist(t, db, "public", "employees", migrations.DuplicationName("fk_employee_department"))
 			},
 			afterRollback: func(t *testing.T, db *sql.DB) {
 			},
--- pkg/migrations/op_set_unique_test.go
+++ pkg/migrations/op_set_unique_test.go
@@ -330,7 +330,7 @@ func TestSetColumnUnique(t *testing.T) {
 			},
 			afterStart: func(t *testing.T, db *sql.DB) {
 				// A temporary FK constraint has been created on the temporary column
-				ValidatedForeignKeyMustExist(t, db, "public", "employees", migrations.TemporaryName("fk_employee_department"))
+				ValidatedForeignKeyMustExist(t, db, "public", "employees", migrations.DuplicationName("fk_employee_department"))
 			},
 			afterRollback: func(t *testing.T, db *sql.DB) {
 			},
