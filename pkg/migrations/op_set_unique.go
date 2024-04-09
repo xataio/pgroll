56f4b7d11842611163ca10c2c6e8823a04ca3033
Andrew Farries <andrew.farries@xata.io>
2024-04-09 14:44:19 +0100

    Update 'set unique' sub-operation

--- pkg/migrations/op_set_unique.go
+++ pkg/migrations/op_set_unique.go
@@ -23,54 +23,10 @@ var _ Operation = (*OpSetUnique)(nil)
 
 func (o *OpSetUnique) Start(ctx context.Context, conn *sql.DB, stateSchema string, tr SQLTransformer, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error) {
 	table := s.GetTable(o.Table)
-	column := table.GetColumn(o.Column)
-
-	// create a copy of the column on the underlying table.
-	d := NewColumnDuplicator(conn, table, column)
-	if err := d.Duplicate(ctx); err != nil {
-		return nil, fmt.Errorf("failed to duplicate column: %w", err)
-	}
 
 	// Add a unique index to the new column
 	if err := o.addUniqueIndex(ctx, conn); err != nil {
 		return nil, fmt.Errorf("failed to add unique index: %w", err)
-	}
-
-	// Add a trigger to copy values from the old column to the new, rewriting values using the `up` SQL.
-	err := createTrigger(ctx, conn, tr, triggerConfig{
-		Name:           TriggerName(o.Table, o.Column),
-		Direction:      TriggerDirectionUp,
-		Columns:        table.Columns,
-		SchemaName:     s.Name,
-		TableName:      o.Table,
-		PhysicalColumn: TemporaryName(o.Column),
-		StateSchema:    stateSchema,
-		SQL:            o.Up,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create up trigger: %w", err)
-	}
-
-	// Add the new column to the internal schema representation. This is done
-	// here, before creation of the down trigger, so that the trigger can declare
-	// a variable for the new column.
-	table.AddColumn(o.Column, schema.Column{
-		Name: TemporaryName(o.Column),
-	})
-
-	// Add a trigger to copy values from the new column to the old, rewriting values using the `down` SQL.
-	err = createTrigger(ctx, conn, tr, triggerConfig{
-		Name:           TriggerName(o.Table, TemporaryName(o.Column)),
-		Direction:      TriggerDirectionDown,
-		Columns:        table.Columns,
-		SchemaName:     s.Name,
-		TableName:      o.Table,
-		PhysicalColumn: o.Column,
-		StateSchema:    stateSchema,
-		SQL:            o.downSQL(),
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create down trigger: %w", err)
 	}
 
 	return table, nil
@@ -86,64 +42,11 @@ func (o *OpSetUnique) Complete(ctx context.Context, conn *sql.DB, tr SQLTransfor
 		return err
 	}
 
-	// Remove the up function and trigger
-	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column)),
-	))
-	if err != nil {
-		return err
-	}
-
-	// Remove the down function and trigger
-	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-		pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column))),
-	))
-	if err != nil {
-		return err
-	}
-
-	// Drop the old column
-	_, err = conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE IF EXISTS %s DROP COLUMN IF EXISTS %s",
-		pq.QuoteIdentifier(o.Table),
-		pq.QuoteIdentifier(o.Column)))
-	if err != nil {
-		return err
-	}
-
-	// Rename the new column to the old column name
-	table := s.GetTable(o.Table)
-	column := table.GetColumn(o.Column)
-	if err := RenameDuplicatedColumn(ctx, conn, table, column); err != nil {
-		return err
-	}
-
 	return err
 }
 
 func (o *OpSetUnique) Rollback(ctx context.Context, conn *sql.DB, tr SQLTransformer) error {
-	// Drop the new column, taking the unique index on the column with it
-	_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE %s DROP COLUMN IF EXISTS %s",
-		pq.QuoteIdentifier(o.Table),
-		pq.QuoteIdentifier(TemporaryName(o.Column)),
-	))
-	if err != nil {
-		return err
-	}
-
-	// Remove the up function and trigger
-	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column)),
-	))
-	if err != nil {
-		return err
-	}
-
-	// Remove the down function and trigger
-	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-		pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column))),
-	))
-
-	return err
+	return nil
 }
 
 func (o *OpSetUnique) Validate(ctx context.Context, s *schema.Schema) error {
@@ -171,13 +74,4 @@ func (o *OpSetUnique) addUniqueIndex(ctx context.Context, conn *sql.DB) error {
 		pq.QuoteIdentifier(TemporaryName(o.Column))))
 
 	return err
-}
-
-// Down SQL is either user-specified or defaults to copying the value from the new column to the old.
-func (o *OpSetUnique) downSQL() string {
-	if o.Down != "" {
-		return o.Down
-	}
-
-	return o.Column
 }
