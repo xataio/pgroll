cb14795c348d4b4422811939811142ea41e9da05
Andrew Farries <andrew.farries@xata.io>
2024-04-12 08:22:46 +0100

    Implement 'set comment' sub-operation

---
+++ pkg/migrations/op_set_comment.go
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package migrations
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/xataio/pgroll/pkg/schema"
+)
+
+type OpSetComment struct {
+	Table   string `json:"table"`
+	Column  string `json:"column"`
+	Comment string `json:"comment"`
+	Up      string `json:"up"`
+	Down    string `json:"down"`
+}
+
+var _ Operation = (*OpSetComment)(nil)
+
+func (o *OpSetComment) Start(ctx context.Context, conn *sql.DB, stateSchema string, tr SQLTransformer, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error) {
+	tbl := s.GetTable(o.Table)
+
+	return tbl, addCommentToColumn(ctx, conn, o.Table, TemporaryName(o.Column), o.Comment)
+}
+
+func (o *OpSetComment) Complete(ctx context.Context, conn *sql.DB, tr SQLTransformer, s *schema.Schema) error {
+	return nil
+}
+
+func (o *OpSetComment) Rollback(ctx context.Context, conn *sql.DB, tr SQLTransformer) error {
+	return nil
+}
+
+func (o *OpSetComment) Validate(ctx context.Context, s *schema.Schema) error {
+	return nil
+}
