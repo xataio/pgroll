xataio/pgroll

commit 0746ba660adac30962aaec1d2bedd858ef4f99ea

Separate DDL operations from DML (backfills) on migration start (#289)
Separate the DDL operations required during migration start from the DML
operations (backfills). Complete all DDL steps before starting DML.

Each `Start` operation returns the name of the table that requires
backfill, if any. After all operations have started, backfills are run
on each table that requires one.

Separating DDL and DML during migration start like this will allow for
setting Postgres options that should apply only for the DDL phase of
migration start (as in #290).

pkg/roll/execute.go
@@ -40,8 +40,9 @@ func (m *Roll) Start(ctx context.Context, migration *migrations.Migration, cbs .
 	}
 
 	// execute operations
+	var tablesToBackfill []*schema.Table
 	for _, op := range migration.Operations {
-		err := op.Start(ctx, m.pgConn, m.state.Schema(), newSchema, cbs...)
+		table, err := op.Start(ctx, m.pgConn, m.state.Schema(), newSchema, cbs...)
 		if err != nil {
 			errRollback := m.Rollback(ctx)
 
@@ -59,6 +60,16 @@ func (m *Roll) Start(ctx context.Context, migration *migrations.Migration, cbs .
 					return fmt.Errorf("unable to refresh schema: %w", err)
 				}
 			}
+		}
+		if table != nil {
+			tablesToBackfill = append(tablesToBackfill, table)
+		}
+	}
+
+	// perform backfill operations for those tables that require it
+	for _, table := range tablesToBackfill {
+		if err := migrations.Backfill(ctx, m.pgConn, table, cbs...); err != nil {
+			return fmt.Errorf("unable to backfill table %q: %w", table.Name, err)
 		}
 	}
 

pkg/migrations/backfill.go
@@ -18,7 +18,7 @@ import (
 // 2. Get the first batch of rows from the table, ordered by the primary key.
 // 3. Update each row in the batch, setting the value of the primary key column to itself.
 // 4. Repeat steps 2 and 3 until no more rows are returned.
-func backfill(ctx context.Context, conn *sql.DB, table *schema.Table, cbs ...CallbackFn) error {
+func Backfill(ctx context.Context, conn *sql.DB, table *schema.Table, cbs ...CallbackFn) error {
 	// get the backfill column
 	identityColumn := getIdentityColumn(table)
 	if identityColumn == nil {

pkg/migrations/migrations.go
@@ -17,7 +17,8 @@ type Operation interface {
 	// Start will apply the required changes to enable supporting the new schema
 	// version in the database (through a view)
 	// update the given views to expose the new schema version
-	Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) error
+	// Returns the table that requires backfilling, if any.
+	Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error)
 
 	// Complete will update the database schema to match the current version
 	// after calling Start.

pkg/migrations/op_add_column.go
@@ -15,31 +15,32 @@ import (
 
 var _ Operation = (*OpAddColumn)(nil)
 
-func (o *OpAddColumn) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) error {
+func (o *OpAddColumn) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error) {
 	table := s.GetTable(o.Table)
 
 	if err := addColumn(ctx, conn, *o, table); err != nil {
-		return fmt.Errorf("failed to start add column operation: %w", err)
+		return nil, fmt.Errorf("failed to start add column operation: %w", err)
 	}
 
 	if o.Column.Comment != nil {
 		if err := addCommentToColumn(ctx, conn, o.Table, TemporaryName(o.Column.Name), *o.Column.Comment); err != nil {
-			return fmt.Errorf("failed to add comment to column: %w", err)
+			return nil, fmt.Errorf("failed to add comment to column: %w", err)
 		}
 	}
 
 	if !o.Column.IsNullable() && o.Column.Default == nil {
 		if err := addNotNullConstraint(ctx, conn, o.Table, o.Column.Name, TemporaryName(o.Column.Name)); err != nil {
-			return fmt.Errorf("failed to add not null constraint: %w", err)
+			return nil, fmt.Errorf("failed to add not null constraint: %w", err)
 		}
 	}
 
 	if o.Column.Check != nil {
 		if err := o.addCheckConstraint(ctx, conn); err != nil {
-			return fmt.Errorf("failed to add check constraint: %w", err)
+			return nil, fmt.Errorf("failed to add check constraint: %w", err)
 		}
 	}
 
+	var tableToBackfill *schema.Table
 	if o.Up != nil {
 		err := createTrigger(ctx, conn, triggerConfig{
 			Name:           TriggerName(o.Table, o.Column.Name),
@@ -52,18 +53,16 @@ func (o *OpAddColumn) Start(ctx context.Context, conn *sql.DB, stateSchema strin
 			SQL:            *o.Up,
 		})
 		if err != nil {
-			return fmt.Errorf("failed to create trigger: %w", err)
+			return nil, fmt.Errorf("failed to create trigger: %w", err)
 		}
-		if err := backfill(ctx, conn, table, cbs...); err != nil {
-			return fmt.Errorf("failed to backfill column: %w", err)
-		}
+		tableToBackfill = table
 	}
 
 	table.AddColumn(o.Column.Name, schema.Column{
 		Name: TemporaryName(o.Column.Name),
 	})
 
-	return nil
+	return tableToBackfill, nil
 }
 
 func (o *OpAddColumn) Complete(ctx context.Context, conn *sql.DB, s *schema.Schema) error {

pkg/migrations/op_alter_column.go
@@ -11,7 +11,7 @@ import (
 
 var _ Operation = (*OpAlterColumn)(nil)
 
-func (o *OpAlterColumn) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) error {
+func (o *OpAlterColumn) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error) {
 	op := o.innerOperation()
 
 	return op.Start(ctx, conn, stateSchema, s, cbs...)

pkg/migrations/op_change_type.go
@@ -21,14 +21,14 @@ type OpChangeType struct {
 
 var _ Operation = (*OpChangeType)(nil)
 
-func (o *OpChangeType) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) error {
+func (o *OpChangeType) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error) {
 	table := s.GetTable(o.Table)
 	column := table.GetColumn(o.Column)
 
 	// Create a copy of the column on the underlying table.
 	d := NewColumnDuplicator(conn, table, column).WithType(o.Type)
 	if err := d.Duplicate(ctx); err != nil {
-		return fmt.Errorf("failed to duplicate column: %w", err)
+		return nil, fmt.Errorf("failed to duplicate column: %w", err)
 	}
 
 	// Add a trigger to copy values from the old column to the new, rewriting values using the `up` SQL.
@@ -43,12 +43,7 @@ func (o *OpChangeType) Start(ctx context.Context, conn *sql.DB, stateSchema stri
 		SQL:            o.Up,
 	})
 	if err != nil {
-		return fmt.Errorf("failed to create up trigger: %w", err)
-	}
-
-	// Backfill the new column with values from the old column.
-	if err := backfill(ctx, conn, table, cbs...); err != nil {
-		return fmt.Errorf("failed to backfill column: %w", err)
+		return nil, fmt.Errorf("failed to create up trigger: %w", err)
 	}
 
 	// Add the new column to the internal schema representation. This is done
@@ -70,10 +65,10 @@ func (o *OpChangeType) Start(ctx context.Context, conn *sql.DB, stateSchema stri
 		SQL:            o.Down,
 	})
 	if err != nil {
-		return fmt.Errorf("failed to create down trigger: %w", err)
+		return nil, fmt.Errorf("failed to create down trigger: %w", err)
 	}
 
-	return nil
+	return table, nil
 }
 
 func (o *OpChangeType) Complete(ctx context.Context, conn *sql.DB, s *schema.Schema) error {

pkg/migrations/op_create_index.go
@@ -14,13 +14,13 @@ import (
 
 var _ Operation = (*OpCreateIndex)(nil)
 
-func (o *OpCreateIndex) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) error {
+func (o *OpCreateIndex) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error) {
 	// create index concurrently
 	_, err := conn.ExecContext(ctx, fmt.Sprintf("CREATE INDEX CONCURRENTLY IF NOT EXISTS %s ON %s (%s)",
 		pq.QuoteIdentifier(o.Name),
 		pq.QuoteIdentifier(o.Table),
 		strings.Join(quoteColumnNames(o.Columns), ", ")))
-	return err
+	return nil, err
 }
 
 func (o *OpCreateIndex) Complete(ctx context.Context, conn *sql.DB, s *schema.Schema) error {

pkg/migrations/op_create_table.go
@@ -13,20 +13,20 @@ import (
 
 var _ Operation = (*OpCreateTable)(nil)
 
-func (o *OpCreateTable) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) error {
+func (o *OpCreateTable) Start(ctx context.Context, conn *sql.DB, stateSchema string, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error) {
 	tempName := TemporaryName(o.Name)
 	_, err := conn.ExecContext(ctx, fmt.Sprintf("CREATE TABLE %s (%s)",
 		pq.QuoteIdentifier(tempName),
 		columnsToSQL(o.Columns)))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Add comments to any columns that have them
 	for _, col := range o.Columns {
 		if col.Comment != nil {
 			if err := addCommentToColumn(ctx, conn, tempName, col.Name, *col.Comment); err != nil {
-				return fmt.Errorf("failed to add comment to column: %w", err)
+				return nil, fmt.Errorf("failed to add comment to column: %w", err)
 			}
 		}
 	}
@@ -34,7 +34,7 @@ func (o *OpCreateTable) Start(ctx context.Context, conn *sql.DB, stateSchema str
 	// Add comment to the table itself
 	if o.Comment != nil {
 		if err := addCommentToTable(ctx, conn, tempName, *o.Comment); err != nil {
-			return fmt.Errorf("failed to add comment to table: %w", err)
+			return nil, fmt.Errorf("failed to add comment to table: %w", err)
 		}
 	}
 
@@ -50,7 +50,7 @@ func (o *OpCreateTable) Start(ctx context.Context, conn *sql.DB, stateSchema str
 		Columns: columns,
 	})
 
-	return nil
+	return nil, nil
 }
 
 func (o *OpCreateTable) Complete(ctx context.Context, conn *sql.DB, s *schema.Schema) error {

commit 2c895e2e0b2e488ff2d7903a413cb8dd7ef766bb

Fix CLI progress updates (#390)
Progress updates showing how many rows have been backfilled during
migration start were added in https://github.com/xataio/pgroll/pull/139.

Progress updates were then broken in
https://github.com/xataio/pgroll/pull/289.

Fix the progress updates to restore the intended behaviour from #139 and
add a test to ensure that callbacks are invoked during migration start.

pkg/roll/execute.go
@@ -21,7 +21,7 @@ func (m *Roll) Start(ctx context.Context, migration *migrations.Migration, cbs .
 	}
 
 	// perform backfills for the tables that require it
-	return m.performBackfills(ctx, tablesToBackfill)
+	return m.performBackfills(ctx, tablesToBackfill, cbs...)
 }
 
 // StartDDLOperations performs the DDL operations for the migration. This does
@@ -265,9 +265,9 @@ func (m *Roll) ensureView(ctx context.Context, version, name string, table schem
 	return nil
 }
 
-func (m *Roll) performBackfills(ctx context.Context, tables []*schema.Table) error {
+func (m *Roll) performBackfills(ctx context.Context, tables []*schema.Table, cbs ...migrations.CallbackFn) error {
 	for _, table := range tables {
-		if err := migrations.Backfill(ctx, m.pgConn, table); err != nil {
+		if err := migrations.Backfill(ctx, m.pgConn, table, cbs...); err != nil {
 			errRollback := m.Rollback(ctx)
 
 			return errors.Join(

pkg/roll/execute_test.go
@@ -637,6 +637,45 @@ func TestMigrationHooksAreInvoked(t *testing.T) {
 	})
 }
 
+func TestCallbacksAreInvokedOnMigrationStart(t *testing.T) {
+	t.Parallel()
+
+	testutils.WithMigratorAndConnectionToContainer(t, func(mig *roll.Roll, db *sql.DB) {
+		ctx := context.Background()
+
+		// Create a table
+		_, err := db.ExecContext(ctx, "CREATE TABLE users (id SERIAL PRIMARY KEY, name text)")
+		require.NoError(t, err)
+
+		// Insert some data
+		_, err = db.ExecContext(ctx,
+			"INSERT INTO users (id, name) VALUES (1, 'alice'), (2, 'bob')")
+		require.NoError(t, err)
+
+		// Define a mock callback
+		invoked := false
+		cb := func(n int64) { invoked = true }
+
+		// Start a migration that requires a backfill
+		err = mig.Start(ctx, &migrations.Migration{
+			Name: "02_change_type",
+			Operations: migrations.Operations{
+				&migrations.OpAlterColumn{
+					Table:  "users",
+					Column: "name",
+					Type:   ptr("varchar(255)"),
+					Up:     "name",
+					Down:   "name",
+				},
+			},
+		}, cb)
+		require.NoError(t, err)
+
+		// Ensure that the callback was invoked
+		assert.True(t, invoked)
+	})
+}
+
 func TestRollSchemaMethodReturnsCorrectSchema(t *testing.T) {
 	t.Parallel()
 