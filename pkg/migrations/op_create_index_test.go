0cab124e0adb49046b52d4033ce071b42c52d9da
Noémi Ványi <kvch@users.noreply.github.com>
2024-10-16 13:59:42 +0200

    Add support for creating partial indexes on tables (#404)
    This PR adds a new option to `create_index` operations called
    `predicate`. When `predicate` is the conditional expression of the
    partial index. When it is set, pgroll creates a partial index on the
    table.
    
    Example:
    ```json
    {
      "name": "37_create_partial_index",
      "operations": [
        {
          "create_index": {
            "name": "idx_fruits_id_gt_10",
            "table": "fruits",
            "columns": [
              "id"
            ],
            "predicate": "id > 10"
          }
        }
      ]
    }
    ```
    
    Closes #322
    
    ---------
    
    Co-authored-by: Ryan Slade <ryanslade@gmail.com>
    Co-authored-by: Andrew Farries <andyrb@gmail.com>

--- pkg/migrations/op_create_index_test.go
+++ pkg/migrations/op_create_index_test.go
@@ -105,6 +105,58 @@ func TestCreateIndex(t *testing.T) {
 				// Complete is a no-op.
 			},
 		},
+		{
+			name: "create partial index",
+			migrations: []migrations.Migration{
+				{
+					Name: "01_add_table",
+					Operations: migrations.Operations{
+						&migrations.OpCreateTable{
+							Name: "users",
+							Columns: []migrations.Column{
+								{
+									Name: "id",
+									Type: "serial",
+									Pk:   ptr(true),
+								},
+								{
+									Name:     "name",
+									Type:     "varchar(255)",
+									Nullable: ptr(false),
+								},
+								{
+									Name:     "registered_at_year",
+									Type:     "integer",
+									Nullable: ptr(false),
+								},
+							},
+						},
+					},
+				},
+				{
+					Name: "02_create_index_for_new_users_after_2019",
+					Operations: migrations.Operations{
+						&migrations.OpCreateIndex{
+							Name:      "idx_users_name_after_2019",
+							Table:     "users",
+							Columns:   []string{"registered_at_year"},
+							Predicate: ptr("registered_at_year > 2019"),
+						},
+					},
+				},
+			},
+			afterStart: func(t *testing.T, db *sql.DB, schema string) {
+				// The index has been created on the underlying table.
+				IndexMustExist(t, db, schema, "users", "idx_users_name_after_2019")
+			},
+			afterRollback: func(t *testing.T, db *sql.DB, schema string) {
+				// The index has been dropped from the the underlying table.
+				IndexMustNotExist(t, db, schema, "users", "idx_users_name_after_2019")
+			},
+			afterComplete: func(t *testing.T, db *sql.DB, schema string) {
+				// Complete is a no-op.
+			},
+		},
 	})
 }
 
