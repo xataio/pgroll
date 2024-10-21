dbe3cb1ddac15b0c64715bf72245bbc773200b6f
Ryan Slade <ryanslade@gmail.com>
2024-10-21 12:31:40 +0200

    Add benchmark for write amplification

--- internal/benchmarks/benchmarks_test.go
+++ internal/benchmarks/benchmarks_test.go
@@ -5,6 +5,7 @@ package benchmarks
 import (
 	"context"
 	"database/sql"
+	"github.com/stretchr/testify/assert"
 	"strconv"
 	"testing"
 	"time"
@@ -18,6 +19,10 @@ import (
 	"github.com/xataio/pgroll/pkg/roll"
 )
 
+const unitRowsPerSecond = "rows/s"
+
+var rowCounts = []int{10_000, 100_000, 1_000_000}
+
 func TestMain(m *testing.M) {
 	testutils.SharedTestMain(m)
 }
@@ -27,77 +32,10 @@ func BenchmarkBackfill(b *testing.B) {
 	testSchema := testutils.TestSchema()
 	var opts []roll.Option
 
-	// Start with a simple table with a nullable `name` field.
-	migCreateTable := migrations.Migration{
-		Name: "01_create_table",
-		Operations: migrations.Operations{
-			&migrations.OpCreateTable{
-				Name: "users",
-				Columns: []migrations.Column{
-					{
-						Name: "id",
-						Type: "serial",
-						Pk:   ptr(true),
-					},
-					{
-						Name:     "name",
-						Type:     "varchar(255)",
-						Nullable: ptr(true),
-						Unique:   ptr(false),
-					},
-				},
-			},
-		},
-	}
-
-	// Alter the table to make the name field not null and backfill the old name fields with
-	// `placeholder`.
-	migAlterColumn := migrations.Migration{
-		Name: "02_alter_column",
-		Operations: migrations.Operations{
-			&migrations.OpAlterColumn{
-				Table:    "users",
-				Column:   "name",
-				Up:       "(SELECT CASE WHEN name IS NULL THEN 'placeholder' ELSE name END)",
-				Down:     "user_name",
-				Comment:  nullable.NewNullableWithValue("the name of the user"),
-				Nullable: ptr(false),
-			},
-		},
-	}
-
-	seed := func(b *testing.B, rowCount int, db *sql.DB) {
-		seedStart := time.Now()
-		defer func() {
-			elapsed := time.Since(seedStart)
-			rowsPerSecond := float64(rowCount) / elapsed.Seconds()
-			b.Logf("Seeded %d rows in %s (%.f rows/s)", rowCount, elapsed, rowsPerSecond)
-		}()
-
-		tx, err := db.Begin()
-		require.NoError(b, err)
-		defer tx.Rollback()
-
-		stmt, err := tx.PrepareContext(ctx, pq.CopyInSchema(testSchema, "users", "name"))
-		require.NoError(b, err)
-
-		for i := 0; i < rowCount; i++ {
-			_, err = stmt.ExecContext(ctx, nil)
-			require.NoError(b, err)
-		}
-
-		_, err = stmt.ExecContext(ctx)
-		require.NoError(b, err)
-		require.NoError(b, tx.Commit())
-	}
-
-	for _, rowCount := range []int{10_000, 100_000, 1_000_000} {
+	for _, rowCount := range rowCounts {
 		b.Run(strconv.Itoa(rowCount), func(b *testing.B) {
 			testutils.WithMigratorInSchemaAndConnectionToContainerWithOptions(b, testSchema, opts, func(mig *roll.Roll, db *sql.DB) {
-				// Setup
-				require.NoError(b, mig.Start(ctx, &migCreateTable))
-				require.NoError(b, mig.Complete(ctx))
-				seed(b, rowCount, db)
+				setupInitialTable(b, ctx, testSchema, mig, db, rowCount)
 				b.ResetTimer()
 
 				// Backfill
@@ -107,11 +45,150 @@ func BenchmarkBackfill(b *testing.B) {
 				b.StopTimer()
 				b.Logf("Backfilled %d rows in %s", rowCount, b.Elapsed())
 				rowsPerSecond := float64(rowCount) / b.Elapsed().Seconds()
-				b.ReportMetric(rowsPerSecond, "rows/s")
+				b.ReportMetric(rowsPerSecond, unitRowsPerSecond)
 				require.NoError(b, mig.Close())
 			})
 		})
 	}
+}
+
+// Benchmark the difference between updating all rows with and without an update trigger in place
+func BenchmarkWriteAmplification(b *testing.B) {
+	ctx := context.Background()
+	testSchema := testutils.TestSchema()
+	var opts []roll.Option
+
+	assertRowCount := func(tb testing.TB, db *sql.DB, rowCount int) {
+		tb.Helper()
+
+		var count int
+		err := db.QueryRowContext(ctx, "SELECT COUNT(*) FROM users WHERE name = 'person'").Scan(&count)
+		assert.NoError(b, err)
+		assert.Equal(b, rowCount, count)
+	}
+
+	b.Run("NoTrigger", func(b *testing.B) {
+		for _, rowCount := range rowCounts {
+			b.Run(strconv.Itoa(rowCount), func(b *testing.B) {
+				testutils.WithMigratorInSchemaAndConnectionToContainerWithOptions(b, testSchema, opts, func(mig *roll.Roll, db *sql.DB) {
+					setupInitialTable(b, ctx, testSchema, mig, db, rowCount)
+					b.ResetTimer()
+
+					// Update the name in all rows
+					b.StartTimer()
+					_, err := db.ExecContext(ctx, `UPDATE users SET name = 'person'`)
+					require.NoError(b, err)
+					b.StopTimer()
+					rowsPerSecond := float64(rowCount) / b.Elapsed().Seconds()
+					b.ReportMetric(rowsPerSecond, unitRowsPerSecond)
+
+					require.NoError(b, mig.Close())
+					assertRowCount(b, db, rowCount)
+				})
+			})
+		}
+	})
+
+	b.Run("WithTrigger", func(b *testing.B) {
+		for _, rowCount := range rowCounts {
+			b.Run(strconv.Itoa(rowCount), func(b *testing.B) {
+				testutils.WithMigratorInSchemaAndConnectionToContainerWithOptions(b, testSchema, opts, func(mig *roll.Roll, db *sql.DB) {
+					setupInitialTable(b, ctx, testSchema, mig, db, rowCount)
+
+					// Start the migration
+					require.NoError(b, mig.Start(ctx, &migAlterColumn))
+
+					b.ResetTimer()
+
+					// Update the name in all rows
+					b.StartTimer()
+					_, err := db.ExecContext(ctx, `UPDATE users SET name = 'person'`)
+					require.NoError(b, err)
+					b.StopTimer()
+					rowsPerSecond := float64(rowCount) / b.Elapsed().Seconds()
+					b.ReportMetric(rowsPerSecond, unitRowsPerSecond)
+
+					require.NoError(b, mig.Complete(ctx))
+
+					require.NoError(b, mig.Close())
+					assertRowCount(b, db, rowCount)
+				})
+			})
+		}
+	})
+}
+
+func setupInitialTable(tb testing.TB, ctx context.Context, testSchema string, mig *roll.Roll, db *sql.DB, rowCount int) {
+	tb.Helper()
+
+	seed := func(tb testing.TB, rowCount int, db *sql.DB) {
+		seedStart := time.Now()
+		defer func() {
+			elapsed := time.Since(seedStart)
+			rowsPerSecond := float64(rowCount) / elapsed.Seconds()
+			tb.Logf("Seeded %d rows in %s (%.f rows/s)", rowCount, elapsed, rowsPerSecond)
+		}()
+
+		tx, err := db.Begin()
+		require.NoError(tb, err)
+		defer tx.Rollback()
+
+		stmt, err := tx.PrepareContext(ctx, pq.CopyInSchema(testSchema, "users", "name"))
+		require.NoError(tb, err)
+
+		for i := 0; i < rowCount; i++ {
+			_, err = stmt.ExecContext(ctx, nil)
+			require.NoError(tb, err)
+		}
+
+		_, err = stmt.ExecContext(ctx)
+		require.NoError(tb, err)
+		require.NoError(tb, tx.Commit())
+	}
+
+	// Setup
+	require.NoError(tb, mig.Start(ctx, &migCreateTable))
+	require.NoError(tb, mig.Complete(ctx))
+	seed(tb, rowCount, db)
+}
+
+// Simple table with a nullable `name` field.
+var migCreateTable = migrations.Migration{
+	Name: "01_create_table",
+	Operations: migrations.Operations{
+		&migrations.OpCreateTable{
+			Name: "users",
+			Columns: []migrations.Column{
+				{
+					Name: "id",
+					Type: "serial",
+					Pk:   ptr(true),
+				},
+				{
+					Name:     "name",
+					Type:     "varchar(255)",
+					Nullable: ptr(true),
+					Unique:   ptr(false),
+				},
+			},
+		},
+	},
+}
+
+// Alter the table to make the name field not null and backfill the old name fields with
+// `placeholder`.
+var migAlterColumn = migrations.Migration{
+	Name: "02_alter_column",
+	Operations: migrations.Operations{
+		&migrations.OpAlterColumn{
+			Table:    "users",
+			Column:   "name",
+			Up:       "(SELECT CASE WHEN name IS NULL THEN 'placeholder' ELSE name END)",
+			Down:     "user_name",
+			Comment:  nullable.NewNullableWithValue("the name of the user"),
+			Nullable: ptr(false),
+		},
+	},
 }
 
 func ptr[T any](x T) *T { return &x }
