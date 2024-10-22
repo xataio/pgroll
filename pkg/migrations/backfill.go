5feb6630ea327312e04bec38aab3089df10906e8
Noémi Ványi <sitbackandwait@gmail.com>
2024-10-22 15:49:56 +0200

    backfilling with multiple pk

--- pkg/migrations/backfill.go
+++ pkg/migrations/backfill.go
@@ -7,6 +7,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/lib/pq"
@@ -22,24 +23,18 @@ import (
 // 4. Repeat steps 2 and 3 until no more rows are returned.
 func Backfill(ctx context.Context, conn db.DB, table *schema.Table, batchSize int, batchDelay time.Duration, cbs ...CallbackFn) error {
 	// get the backfill column
-	identityColumn := getIdentityColumn(table)
-	if identityColumn == nil {
+	identityColumns := getIdentityColumns(table)
+	if identityColumns == nil {
 		return BackfillNotPossibleError{Table: table.Name}
 	}
 
 	// Create a batcher for the table.
-	b := batcher{
-		table:          table,
-		identityColumn: identityColumn,
-		lastValue:      nil,
-		batchSize:      batchSize,
-		batchDelay:     batchDelay,
-	}
+	b := newBatcher(table, batchSize)
 
 	// Update each batch of rows, invoking callbacks for each one.
 	for batch := 0; ; batch++ {
 		for _, cb := range cbs {
-			cb(int64(batch * b.batchSize))
+			cb(int64(batch * batchSize))
 		}
 
 		if err := b.updateBatch(ctx, conn); err != nil {
@@ -49,7 +44,7 @@ func Backfill(ctx context.Context, conn db.DB, table *schema.Table, batchSize in
 			return err
 		}
 
-		time.Sleep(b.batchDelay)
+		time.Sleep(batchDelay)
 	}
 
 	return nil
@@ -57,8 +52,8 @@ func Backfill(ctx context.Context, conn db.DB, table *schema.Table, batchSize in
 
 // checkBackfill will return an error if the backfill operation is not supported.
 func checkBackfill(table *schema.Table) error {
-	col := getIdentityColumn(table)
-	if col == nil {
+	cols := getIdentityColumns(table)
+	if cols == nil {
 		return BackfillNotPossibleError{Table: table.Name}
 	}
 
@@ -66,16 +61,20 @@ func checkBackfill(table *schema.Table) error {
 }
 
 // getIdentityColumn will return a column suitable for use in a backfill operation.
-func getIdentityColumn(table *schema.Table) *schema.Column {
+func getIdentityColumns(table *schema.Table) []string {
 	pks := table.GetPrimaryKey()
-	if len(pks) == 1 {
-		return pks[0]
+	if len(pks) != 0 {
+		pkNames := make([]string, len(pks))
+		for i, pk := range pks {
+			pkNames[i] = pk.Name
+		}
+		return pkNames
 	}
 
 	// If there is no primary key, look for a unique not null column
 	for _, col := range table.Columns {
 		if col.Unique && !col.Nullable {
-			return &col
+			return []string{col.Name}
 		}
 	}
 
@@ -84,21 +83,26 @@ func getIdentityColumn(table *schema.Table) *schema.Column {
 }
 
 type batcher struct {
-	table          *schema.Table
-	identityColumn *schema.Column
-	lastValue      *string
-	batchSize      int
-	batchDelay     time.Duration
+	statementBuilder *batchStatementBuilder
+	lastValues       []string
+	batchSize        int
+}
+
+func newBatcher(table *schema.Table, batchSize int) *batcher {
+	return &batcher{
+		statementBuilder: newBatchStatementBuilder(table.Name, getIdentityColumns(table), batchSize),
+		lastValues:       nil,
+	}
 }
 
 func (b *batcher) updateBatch(ctx context.Context, conn db.DB) error {
 	return conn.WithRetryableTransaction(ctx, func(ctx context.Context, tx *sql.Tx) error {
 		// Build the query to update the next batch of rows
-		query := b.buildQuery()
+		query := b.statementBuilder.buildQuery(b.lastValues)
 
 		// Execute the query to update the next batch of rows and update the last PK
 		// value for the next batch
-		err := tx.QueryRowContext(ctx, query).Scan(&b.lastValue)
+		err := tx.QueryRowContext(ctx, query).Scan(&b.lastValues)
 		if err != nil {
 			return err
 		}
@@ -107,23 +111,72 @@ func (b *batcher) updateBatch(ctx context.Context, conn db.DB) error {
 	})
 }
 
+type batchStatementBuilder struct {
+	tableName       string
+	identityColumns []string
+	batchSize       int
+}
+
+func newBatchStatementBuilder(tableName string, identityColumnNames []string, batchSize int) *batchStatementBuilder {
+	quotedCols := make([]string, len(identityColumnNames))
+	for i, col := range identityColumnNames {
+		quotedCols[i] = pq.QuoteIdentifier(col)
+	}
+	return &batchStatementBuilder{
+		tableName:       pq.QuoteIdentifier(tableName),
+		identityColumns: quotedCols,
+		batchSize:       batchSize,
+	}
+}
+
 // buildQuery builds the query used to update the next batch of rows.
-func (b *batcher) buildQuery() string {
+func (sb *batchStatementBuilder) buildQuery(lastValues []string) string {
+	return fmt.Sprintf("WITH batch AS (%[1]s), update AS (%[2]s) %[3]s",
+		sb.buildBatchSubQuery(lastValues),
+		sb.buildUpdateBatchSubQuery(),
+		sb.buildLastValueQuery())
+}
+
+// fetch the next batch of PK of rows to update
+func (sb *batchStatementBuilder) buildBatchSubQuery(lastValues []string) string {
 	whereClause := ""
-	if b.lastValue != nil {
-		whereClause = fmt.Sprintf("WHERE %s > %v", pq.QuoteIdentifier(b.identityColumn.Name), pq.QuoteLiteral(*b.lastValue))
+	if lastValues != nil {
+		conditions := make([]string, len(sb.identityColumns))
+		for i, col := range sb.identityColumns {
+			conditions[i] = fmt.Sprintf("%s > %s ", col, lastValues[i])
+		}
+		whereClause = "WHERE " + strings.Join(conditions, "AND ")
 	}
 
-	return fmt.Sprintf(`
-    WITH batch AS (
-      SELECT %[1]s FROM %[2]s %[4]s ORDER BY %[1]s LIMIT %[3]d FOR NO KEY UPDATE
-    ), update AS (
-      UPDATE %[2]s SET %[1]s=%[2]s.%[1]s FROM batch WHERE %[2]s.%[1]s = batch.%[1]s RETURNING %[2]s.%[1]s
-    )
-    SELECT LAST_VALUE(%[1]s) OVER() FROM update
-    `,
-		pq.QuoteIdentifier(b.identityColumn.Name),
-		pq.QuoteIdentifier(b.table.Name),
-		b.batchSize,
-		whereClause)
+	return fmt.Sprintf("SELECT %[1]s FROM %[2]s %[3]sORDER BY %[1]s LIMIT %[4]d FOR NO KEY UPDATE",
+		strings.Join(sb.identityColumns, ", "), sb.tableName, whereClause, sb.batchSize)
+}
+
+// update the rows in the batch
+func (sb *batchStatementBuilder) buildUpdateBatchSubQuery() string {
+	conditions := make([]string, len(sb.identityColumns))
+	for i, col := range sb.identityColumns {
+		conditions[i] = fmt.Sprintf("%[1]s.%[2]s = batch.%[2]s", sb.tableName, col)
+	}
+	updateWhereClause := "WHERE " + strings.Join(conditions, " AND ")
+
+	setStmt := fmt.Sprintf("%[1]s = %[2]s.%[1]s", sb.identityColumns[0], sb.tableName)
+	for i := 1; i < len(sb.identityColumns); i++ {
+		setStmt += fmt.Sprintf(", %[1]s = %[2]s.%[1]s", sb.identityColumns[i], sb.tableName)
+	}
+	updateReturning := sb.tableName + "." + sb.identityColumns[0]
+	for i := 1; i < len(sb.identityColumns); i++ {
+		updateReturning += ", " + sb.tableName + "." + sb.identityColumns[i]
+	}
+	return fmt.Sprintf("UPDATE %[1]s SET %[2]s FROM batch %[3]s RETURNING %[4]s",
+		sb.tableName, setStmt, updateWhereClause, updateReturning)
+}
+
+// fetch the last values of the PK column
+func (sb *batchStatementBuilder) buildLastValueQuery() string {
+	lastValues := make([]string, len(sb.identityColumns))
+	for i, col := range sb.identityColumns {
+		lastValues[i] = "LAST_VALUE(" + col + ")"
+	}
+	return fmt.Sprintf("SELECT %[1]s OVER() FROM update", strings.Join(lastValues, ", "))
 }
