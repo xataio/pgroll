fa10ad9fd8d7c36706d43e5b61156982d40620f2
Ryan Slade <ryanslade@gmail.com>
2024-10-29 09:44:31 +0100

    Add test for create index

--- pkg/migrations/op_create_index_test.go
+++ pkg/migrations/op_create_index_test.go
@@ -4,6 +4,7 @@ package migrations_test
 
 import (
 	"database/sql"
+	"strings"
 	"testing"
 
 	"github.com/xataio/pgroll/pkg/migrations"
@@ -12,6 +13,7 @@ import (
 func TestCreateIndex(t *testing.T) {
 	t.Parallel()
 
+	invalidName := strings.Repeat("x", 64)
 	ExecuteTests(t, TestCases{
 		{
 			name: "create index",
@@ -156,6 +158,50 @@ func TestCreateIndex(t *testing.T) {
 			afterComplete: func(t *testing.T, db *sql.DB, schema string) {
 				// Complete is a no-op.
 			},
+		},
+		{
+			name: "invalid name",
+			migrations: []migrations.Migration{
+				{
+					Name: "01_add_table",
+					Operations: migrations.Operations{
+						&migrations.OpCreateTable{
+							Name: "users",
+							Columns: []migrations.Column{
+								{
+									Name: "id",
+									Type: "serial",
+									Pk:   ptr(true),
+								},
+								{
+									Name:     "name",
+									Type:     "varchar(255)",
+									Nullable: ptr(false),
+								},
+								{
+									Name:     "registered_at_year",
+									Type:     "integer",
+									Nullable: ptr(false),
+								},
+							},
+						},
+					},
+				},
+				{
+					Name: "02_create_index_with_invalid_name",
+					Operations: migrations.Operations{
+						&migrations.OpCreateIndex{
+							Name:    invalidName,
+							Table:   "users",
+							Columns: []string{"registered_at_year"},
+						},
+					},
+				},
+			},
+			wantStartErr:  migrations.ValidateIdentifierLength(invalidName),
+			afterStart:    func(t *testing.T, db *sql.DB, schema string) {},
+			afterRollback: func(t *testing.T, db *sql.DB, schema string) {},
+			afterComplete: func(t *testing.T, db *sql.DB, schema string) {},
 		},
 	})
 }
