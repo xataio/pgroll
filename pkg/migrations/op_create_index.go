f1fde35a2f39bf4f42a6fae26e5f21bf6f39cf6c
Andrew Farries <andrew.farries@xata.io>
2024-11-06 09:35:18 +0000

    Support adding an index on a newly created table
    Ensure that the table is referred to by the its name in the virtual
    schema - this is aware of the temporary name that is used for the
    table in the migration.

--- pkg/migrations/op_create_index.go
+++ pkg/migrations/op_create_index.go
@@ -16,6 +16,8 @@ import (
 var _ Operation = (*OpCreateIndex)(nil)
 
 func (o *OpCreateIndex) Start(ctx context.Context, conn db.DB, latestSchema string, tr SQLTransformer, s *schema.Schema, cbs ...CallbackFn) (*schema.Table, error) {
+	table := s.GetTable(o.Table)
+
 	// create index concurrently
 	stmtFmt := "CREATE INDEX CONCURRENTLY %s ON %s"
 	if o.Unique != nil && *o.Unique {
@@ -23,7 +25,7 @@ func (o *OpCreateIndex) Start(ctx context.Context, conn db.DB, latestSchema stri
 	}
 	stmt := fmt.Sprintf(stmtFmt,
 		pq.QuoteIdentifier(o.Name),
-		pq.QuoteIdentifier(o.Table))
+		pq.QuoteIdentifier(table.Name))
 
 	if o.Method != nil {
 		stmt += fmt.Sprintf(" USING %s", string(*o.Method))
