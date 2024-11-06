0ddf44660a69def13bc91d40e9fc1a18d79dbab1
Noémi Ványi <sitbackandwait@gmail.com>
2024-11-06 12:32:34 +0100

    more rewrite fix

--- pkg/migrations/op_add_column.go
+++ pkg/migrations/op_add_column.go
@@ -238,7 +238,7 @@ func (o *OpAddColumn) addCheckConstraint(ctx context.Context, conn db.DB) error 
 	_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE %s ADD CONSTRAINT %s CHECK (%s) NOT VALID",
 		pq.QuoteIdentifier(o.Table),
 		pq.QuoteIdentifier(o.Column.Check.Name),
-		rewriteCheckExpression(o.Column.Check.Constraint, o.Column.Name, TemporaryName(o.Column.Name)),
+		rewriteCheckExpression(o.Column.Check.Constraint, o.Column.Name),
 	))
 	return err
 }
