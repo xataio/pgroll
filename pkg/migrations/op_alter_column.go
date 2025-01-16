6aaa601ffa273c0e230e3b79d9be7700f3c81d2c
Andrew Farries <andrew.farries@xata.io>
2025-01-16 14:55:39 +0000

    Remove all uses of `OpAlterColumn.Name`

--- pkg/migrations/op_alter_column.go
+++ pkg/migrations/op_alter_column.go
@@ -5,7 +5,6 @@ package migrations
 import (
 	"context"
 	"fmt"
-	"maps"
 
 	"github.com/lib/pq"
 
@@ -21,11 +20,9 @@ func (o *OpAlterColumn) Start(ctx context.Context, conn db.DB, latestSchema stri
 	ops := o.subOperations()
 
 	// Duplicate the column on the underlying table.
-	if !o.IsRenameOnly() {
-		d := duplicatorForOperations(ops, conn, table, column)
-		if err := d.Duplicate(ctx); err != nil {
-			return nil, fmt.Errorf("failed to duplicate column: %w", err)
-		}
+	d := duplicatorForOperations(ops, conn, table, column)
+	if err := d.Duplicate(ctx); err != nil {
+		return nil, fmt.Errorf("failed to duplicate column: %w", err)
 	}
 
 	// perform any operation specific start steps
@@ -36,63 +33,42 @@ func (o *OpAlterColumn) Start(ctx context.Context, conn db.DB, latestSchema stri
 	}
 
 	// Add a trigger to copy values from the old column to the new, rewriting values using the `up` SQL.
-	// Rename column operations do not require this trigger.
-	if !o.IsRenameOnly() {
-		err := createTrigger(ctx, conn, tr, triggerConfig{
-			Name:           TriggerName(o.Table, o.Column),
-			Direction:      TriggerDirectionUp,
-			Columns:        table.Columns,
-			SchemaName:     s.Name,
-			LatestSchema:   latestSchema,
-			TableName:      o.Table,
-			PhysicalColumn: TemporaryName(o.Column),
-			SQL:            o.upSQLForOperations(ops),
-		})
-		if err != nil {
-			return nil, fmt.Errorf("failed to create up trigger: %w", err)
-		}
-
-		// Add the new column to the internal schema representation. This is done
-		// here, before creation of the down trigger, so that the trigger can declare
-		// a variable for the new column.
-		table.AddColumn(o.Column, &schema.Column{
-			Name: TemporaryName(o.Column),
-		})
-
-		// If the column has been renamed, temporarily update the column name in
-		// the internal schema representation to ensure that the variable name in
-		// the down trigger corresponds to the new name of column.
-		cols := table.Columns
-		if o.Name != nil {
-			cols = maps.Clone(table.Columns)
-			cols[*o.Name] = cols[o.Column]
-			delete(cols, o.Column)
-		}
-
-		// Add a trigger to copy values from the new column to the old.
-		err = createTrigger(ctx, conn, tr, triggerConfig{
-			Name:           TriggerName(o.Table, TemporaryName(o.Column)),
-			Direction:      TriggerDirectionDown,
-			Columns:        cols,
-			LatestSchema:   latestSchema,
-			SchemaName:     s.Name,
-			TableName:      o.Table,
-			PhysicalColumn: o.Column,
-			SQL:            o.downSQLForOperations(ops),
-		})
-		if err != nil {
-			return nil, fmt.Errorf("failed to create down trigger: %w", err)
-		}
+	err := createTrigger(ctx, conn, tr, triggerConfig{
+		Name:           TriggerName(o.Table, o.Column),
+		Direction:      TriggerDirectionUp,
+		Columns:        table.Columns,
+		SchemaName:     s.Name,
+		LatestSchema:   latestSchema,
+		TableName:      o.Table,
+		PhysicalColumn: TemporaryName(o.Column),
+		SQL:            o.upSQLForOperations(ops),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create up trigger: %w", err)
 	}
 
-	// rename the column in the virtual schema if required
-	if o.Name != nil {
-		table.RenameColumn(o.Column, *o.Name)
+	// Add the new column to the internal schema representation. This is done
+	// here, before creation of the down trigger, so that the trigger can declare
+	// a variable for the new column.
+	table.AddColumn(o.Column, &schema.Column{
+		Name: TemporaryName(o.Column),
+	})
+
+	// Add a trigger to copy values from the new column to the old.
+	err = createTrigger(ctx, conn, tr, triggerConfig{
+		Name:           TriggerName(o.Table, TemporaryName(o.Column)),
+		Direction:      TriggerDirectionDown,
+		Columns:        table.Columns,
+		LatestSchema:   latestSchema,
+		SchemaName:     s.Name,
+		TableName:      o.Table,
+		PhysicalColumn: o.Column,
+		SQL:            o.downSQLForOperations(ops),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create down trigger: %w", err)
 	}
 
-	if o.IsRenameOnly() {
-		return nil, nil
-	}
 	return table, nil
 }
 
@@ -106,46 +82,33 @@ func (o *OpAlterColumn) Complete(ctx context.Context, conn db.DB, tr SQLTransfor
 		}
 	}
 
-	if !o.IsRenameOnly() {
-		// Drop the old column
-		_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE IF EXISTS %s DROP COLUMN IF EXISTS %s",
-			pq.QuoteIdentifier(o.Table),
-			pq.QuoteIdentifier(o.Column)))
-		if err != nil {
-			return err
-		}
-
-		// Remove the up function and trigger
-		_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-			pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column))))
-		if err != nil {
-			return err
-		}
-
-		// Remove the down function and trigger
-		_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-			pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column)))))
-		if err != nil {
-			return err
-		}
-
-		// Rename the new column to the old column name
-		table := s.GetTable(o.Table)
-		column := table.GetColumn(o.Column)
-		if err := RenameDuplicatedColumn(ctx, conn, table, column); err != nil {
-			return err
-		}
+	// Drop the old column
+	_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE IF EXISTS %s DROP COLUMN IF EXISTS %s",
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(o.Column)))
+	if err != nil {
+		return err
 	}
 
-	// rename the column in the underlying table if required
-	if o.Name != nil {
-		_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE %s RENAME COLUMN %s TO %s",
-			pq.QuoteIdentifier(o.Table),
-			pq.QuoteIdentifier(o.Column),
-			pq.QuoteIdentifier(*o.Name)))
-		if err != nil {
-			return err
-		}
+	// Remove the up function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column))))
+	if err != nil {
+		return err
+	}
+
+	// Remove the down function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column)))))
+	if err != nil {
+		return err
+	}
+
+	// Rename the new column to the old column name
+	table := s.GetTable(o.Table)
+	column := table.GetColumn(o.Column)
+	if err := RenameDuplicatedColumn(ctx, conn, table, column); err != nil {
+		return err
 	}
 
 	return nil
@@ -153,7 +116,6 @@ func (o *OpAlterColumn) Complete(ctx context.Context, conn db.DB, tr SQLTransfor
 
 func (o *OpAlterColumn) Rollback(ctx context.Context, conn db.DB, tr SQLTransformer, s *schema.Schema) error {
 	ops := o.subOperations()
-	table := s.GetTable(o.Table)
 
 	// Perform any operation specific rollback steps
 	for _, ops := range ops {
@@ -162,37 +124,29 @@ func (o *OpAlterColumn) Rollback(ctx context.Context, conn db.DB, tr SQLTransfor
 		}
 	}
 
-	if !o.IsRenameOnly() {
-		// Drop the new column
-		_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE %s DROP COLUMN IF EXISTS %s",
-			pq.QuoteIdentifier(o.Table),
-			pq.QuoteIdentifier(TemporaryName(o.Column)),
-		))
-		if err != nil {
-			return err
-		}
-
-		// Remove the up function and trigger
-		_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-			pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column)),
-		))
-		if err != nil {
-			return err
-		}
-
-		// Remove the down function and trigger
-		_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
-			pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column))),
-		))
-		if err != nil {
-			return err
-		}
+	// Drop the new column
+	_, err := conn.ExecContext(ctx, fmt.Sprintf("ALTER TABLE %s DROP COLUMN IF EXISTS %s",
+		pq.QuoteIdentifier(o.Table),
+		pq.QuoteIdentifier(TemporaryName(o.Column)),
+	))
+	if err != nil {
+		return err
 	}
 
-	// Rename the column back to its original name in the virtual schema if
-	// required
-	if o.Name != nil {
-		table.RenameColumn(*o.Name, o.Column)
+	// Remove the up function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, o.Column)),
+	))
+	if err != nil {
+		return err
+	}
+
+	// Remove the down function and trigger
+	_, err = conn.ExecContext(ctx, fmt.Sprintf("DROP FUNCTION IF EXISTS %s CASCADE",
+		pq.QuoteIdentifier(TriggerFunctionName(o.Table, TemporaryName(o.Column))),
+	))
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -210,38 +164,15 @@ func (o *OpAlterColumn) Validate(ctx context.Context, s *schema.Schema) error {
 
 	// If the operation requires backfills (ie it isn't a rename-only operation),
 	// ensure that the column meets the requirements for backfilling.
-	if !o.IsRenameOnly() {
-		if err := checkBackfill(table); err != nil {
-			return err
-		}
-	}
-
-	// If the column is being renamed, ensure that the target column name does
-	// not already exist.
-	if o.Name != nil {
-		if table.GetColumn(*o.Name) != nil {
-			return ColumnAlreadyExistsError{Table: o.Table, Name: *o.Name}
-		}
-		if err := ValidateIdentifierLength(*o.Name); err != nil {
-			return err
-		}
+	if err := checkBackfill(table); err != nil {
+		return err
 	}
 
 	ops := o.subOperations()
 
 	// Ensure that at least one sub-operation or rename is present
-	if len(ops) == 0 && o.Name == nil {
+	if len(ops) == 0 {
 		return AlterColumnNoChangesError{Table: o.Table, Column: o.Column}
-	}
-
-	// Rename-only operations are not allowed to have `up` or `down` SQL
-	if o.IsRenameOnly() {
-		if o.Up != "" {
-			return NoUpSQLAllowedError{}
-		}
-		if o.Down != "" {
-			return NoDownSQLAllowedError{}
-		}
 	}
 
 	// Validate the sub-operations in isolation
@@ -249,12 +180,6 @@ func (o *OpAlterColumn) Validate(ctx context.Context, s *schema.Schema) error {
 		if err := op.Validate(ctx, s); err != nil {
 			return err
 		}
-	}
-
-	// Rename the column in the virtual schema if required so that subsequent
-	// operations can validate using the new column name
-	if o.Name != nil {
-		table.RenameColumn(o.Column, *o.Name)
 	}
 
 	return nil
@@ -395,9 +320,4 @@ func (o *OpAlterColumn) upSQLForOperations(ops []Operation) string {
 	}
 
 	return ""
-}
-
-// IsRenameOnly returns true if the operation is a rename column operation only.
-func (o *OpAlterColumn) IsRenameOnly() bool {
-	return len(o.subOperations()) == 0 && o.Name != nil
 }
