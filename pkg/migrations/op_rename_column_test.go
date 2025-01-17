895afe45717f706efcd9cc2a4fda9b02a941c656
Andrew Farries <andyrb@gmail.com>
2025-01-17 15:08:09 +0000

    Add `OpRenameColumn` for renaming columns (#602)
    Add a new `rename_column` operation for performing column renames:
    
    ```json
    {
      "name": "13_rename_column",
      "operations": [
        {
          "rename_column": {
            "table": "employees",
            "from": "role",
            "to": "job_title"
          }
        }
      ]
    }
    ```
    
    Renaming columns is currently done using the `alter_column` operation:
    
    ```json
    {
      "name": "13_rename_column",
      "operations": [
        {
          "alter_column": {
            "table": "employees",
            "column": "role",
            "name": "job_title"
          }
        }
      ]
    }
    ```
    
    As of this PR both approaches are valid, but the second approach (using
    `alter_column` to perform column renames) will be removed as part of
    #601
    
    Part of #601

--- pkg/migrations/op_rename_column_test.go
+++ pkg/migrations/op_rename_column_test.go
@@ -98,3 +98,159 @@ func TestRenameColumn(t *testing.T) {
 		wantStartErr: migrations.ColumnAlreadyExistsError{Table: "users", Name: "id"},
 	}})
 }
+
+func TestOpRenameColumn(t *testing.T) {
+	t.Parallel()
+
+	ExecuteTests(t, TestCases{
+		{
+			name: "rename column",
+			migrations: []migrations.Migration{
+				{
+					Name: "01_create_table",
+					Operations: migrations.Operations{
+						&migrations.OpCreateTable{
+							Name: "users",
+							Columns: []migrations.Column{
+								{
+									Name: "id",
+									Type: "serial",
+									Pk:   true,
+								},
+								{
+									Name:     "username",
+									Type:     "varchar(255)",
+									Nullable: false,
+								},
+							},
+						},
+					},
+				},
+				{
+					Name: "02_rename_column",
+					Operations: migrations.Operations{
+						&migrations.OpRenameColumn{
+							Table: "users",
+							From:  "username",
+							To:    "name",
+						},
+					},
+				},
+			},
+			afterStart: func(t *testing.T, db *sql.DB, schema string) {
+				// The column in the underlying table has not been renamed.
+				ColumnMustExist(t, db, schema, "users", "username")
+
+				// Insertions to the new column name in the new version schema should work.
+				MustInsert(t, db, schema, "02_rename_column", "users", map[string]string{
+					"name": "alice",
+				})
+
+				// Insertions to the old column name in the old version schema should work.
+				MustInsert(t, db, schema, "01_create_table", "users", map[string]string{
+					"username": "bob",
+				})
+
+				// Data can be read from the view in the new version schema.
+				rows := MustSelect(t, db, schema, "02_rename_column", "users")
+				assert.Equal(t, []map[string]any{
+					{"id": 1, "name": "alice"},
+					{"id": 2, "name": "bob"},
+				}, rows)
+			},
+			afterRollback: func(t *testing.T, db *sql.DB, schema string) {
+				// no-op
+			},
+			afterComplete: func(t *testing.T, db *sql.DB, schema string) {
+				// The column in the underlying table has been renamed.
+				ColumnMustExist(t, db, schema, "users", "name")
+
+				// Data can be read from the view in the new version schema.
+				rows := MustSelect(t, db, schema, "02_rename_column", "users")
+				assert.Equal(t, []map[string]any{
+					{"id": 1, "name": "alice"},
+					{"id": 2, "name": "bob"},
+				}, rows)
+			},
+		},
+	})
+}
+
+func TestOpRenameColumnValidation(t *testing.T) {
+	t.Parallel()
+
+	createTableMigration := migrations.Migration{
+		Name: "01_create_table",
+		Operations: migrations.Operations{
+			&migrations.OpCreateTable{
+				Name: "users",
+				Columns: []migrations.Column{
+					{
+						Name: "id",
+						Type: "serial",
+						Pk:   true,
+					},
+					{
+						Name:     "username",
+						Type:     "varchar(255)",
+						Nullable: false,
+					},
+				},
+			},
+		},
+	}
+
+	ExecuteTests(t, TestCases{
+		{
+			name: "table must exist",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				{
+					Name: "02_rename_column",
+					Operations: migrations.Operations{
+						&migrations.OpRenameColumn{
+							Table: "doesntexist",
+							From:  "username",
+							To:    "id",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.TableDoesNotExistError{Name: "doesntexist"},
+		},
+		{
+			name: "source column must exist",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				{
+					Name: "02_rename_column",
+					Operations: migrations.Operations{
+						&migrations.OpRenameColumn{
+							Table: "users",
+							From:  "doesntexist",
+							To:    "id",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.ColumnDoesNotExistError{Table: "users", Name: "doesntexist"},
+		},
+		{
+			name: "target column must not exist",
+			migrations: []migrations.Migration{
+				createTableMigration,
+				{
+					Name: "02_rename_column",
+					Operations: migrations.Operations{
+						&migrations.OpRenameColumn{
+							Table: "users",
+							From:  "username",
+							To:    "id",
+						},
+					},
+				},
+			},
+			wantStartErr: migrations.ColumnAlreadyExistsError{Table: "users", Name: "id"},
+		},
+	})
+}
