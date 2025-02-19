0e3748f759ab54caa04e5438443c8b318d4858f3
Noémi Ványi <sitbackandwait@gmail.com>
2025-02-19 15:02:42 +0100

    Support generating list of pgroll operations from multiple SQL statements

--- pkg/sql2pgroll/convert.go
+++ pkg/sql2pgroll/convert.go
@@ -10,52 +10,57 @@ import (
 	"github.com/xataio/pgroll/pkg/migrations"
 )
 
-var ErrStatementCount = fmt.Errorf("expected exactly one statement")
-
 // Convert converts a SQL statement to a slice of pgroll operations.
 func Convert(sql string) (migrations.Operations, error) {
-	ops, err := convert(sql)
-	if err != nil {
-		return nil, err
-	}
-
-	if ops == nil {
-		return makeRawSQLOperation(sql), nil
-	}
-
-	return ops, nil
-}
-
-func convert(sql string) (migrations.Operations, error) {
 	tree, err := pgq.Parse(sql)
 	if err != nil {
 		return nil, fmt.Errorf("parse error: %w", err)
 	}
 
+	var migOps migrations.Operations
 	stmts := tree.GetStmts()
-	if len(stmts) != 1 {
-		return nil, fmt.Errorf("%w: got %d statements", ErrStatementCount, len(stmts))
+	for i, stmt := range stmts {
+		if stmt.GetStmt() == nil {
+			continue
+		}
+		node := stmts[i].GetStmt().GetNode()
+		var ops migrations.Operations
+		var err error
+		switch node := (node).(type) {
+		case *pgq.Node_CreateStmt:
+			ops, err = convertCreateStmt(node.CreateStmt)
+		case *pgq.Node_AlterTableStmt:
+			ops, err = convertAlterTableStmt(node.AlterTableStmt)
+		case *pgq.Node_RenameStmt:
+			ops, err = convertRenameStmt(node.RenameStmt)
+		case *pgq.Node_DropStmt:
+			ops, err = convertDropStatement(node.DropStmt)
+		case *pgq.Node_IndexStmt:
+			ops, err = convertCreateIndexStmt(node.IndexStmt)
+		default:
+			// SQL statement cannot be transformed to pgroll operation
+			// so we will use raw SQL operation
+			ops = makeRawSQLOperation(sql, i)
+		}
+		if err != nil {
+			return nil, err
+		}
+		if ops == nil {
+			ops = makeRawSQLOperation(sql, i)
+		}
+		migOps = append(migOps, ops...)
 	}
-	node := stmts[0].GetStmt().GetNode()
-
-	switch node := (node).(type) {
-	case *pgq.Node_CreateStmt:
-		return convertCreateStmt(node.CreateStmt)
-	case *pgq.Node_AlterTableStmt:
-		return convertAlterTableStmt(node.AlterTableStmt)
-	case *pgq.Node_RenameStmt:
-		return convertRenameStmt(node.RenameStmt)
-	case *pgq.Node_DropStmt:
-		return convertDropStatement(node.DropStmt)
-	case *pgq.Node_IndexStmt:
-		return convertCreateIndexStmt(node.IndexStmt)
-	default:
-		return makeRawSQLOperation(sql), nil
-	}
+	return migOps, nil
 }
 
-func makeRawSQLOperation(sql string) migrations.Operations {
+func makeRawSQLOperation(sql string, idx int) migrations.Operations {
+	stmts, err := pgq.SplitWithParser(sql, true)
+	if err != nil {
+		return migrations.Operations{
+			&migrations.OpRawSQL{Up: sql},
+		}
+	}
 	return migrations.Operations{
-		&migrations.OpRawSQL{Up: sql},
+		&migrations.OpRawSQL{Up: stmts[idx]},
 	}
 }
