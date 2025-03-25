a11130432664145ded41366e294eb94addd1bcbf
Noémi Ványi <kvch@users.noreply.github.com>
2025-03-25 13:20:41 +0100

    Implement creating a unique index concurrently as a `DBAction`

--- pkg/migrations/op_set_unique.go
+++ pkg/migrations/op_set_unique.go
@@ -30,8 +30,13 @@ func (o *OpSetUnique) Start(ctx context.Context, conn db.DB, latestSchema string
 	if column == nil {
 		return nil, ColumnDoesNotExistError{Table: o.Table, Name: o.Column}
 	}
+	createIndex := NewCreateUniqueIndexConcurrentlyAction(conn, s.Name, o.Name, table.Name, column.Name)
+	err := createIndex.Execute(ctx)
+	if err != nil {
+		return table, fmt.Errorf("failed to create unique index: %w", err)
+	}
 
-	return table, createUniqueIndexConcurrently(ctx, conn, s.Name, o.Name, table.Name, []string{column.Name})
+	return table, nil
 }
 
 func (o *OpSetUnique) Complete(ctx context.Context, conn db.DB, s *schema.Schema) error {
