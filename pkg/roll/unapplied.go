efb95f3a25ceb717ee592ef30b5145d9d0c59702
Andrew Farries <andrew.farries@xata.io>
2025-05-03 17:34:36 +0100

    Implement `UnappliedMigrations` baseline handling
    Make the `UnappliedMigrations` method aware of baselines.
    
    Only migrations that come after the most recent baseline should be
    considered unapplied; the baseline and all preceding migrations are
    never considered unapplied as they are no longer part of the schema
    history.

--- pkg/roll/unapplied.go
+++ pkg/roll/unapplied.go
@@ -6,13 +6,14 @@ import (
 	"context"
 	"fmt"
 	"io/fs"
+	"sort"
 
 	"github.com/xataio/pgroll/pkg/migrations"
 )
 
-// UnappliedMigrations returns a slice of unapplied migrations from `dir`,
-// lexicographically ordered by filename without extension. Applying each of the returned
-// migrations in order will bring the database up to date with `dir`.
+// UnappliedMigrations returns the slice of unapplied migrations from `dir`
+// that have not yet been applied to the database. Applying each of the
+// returned migrations in order will bring the database up to date with `dir`.
 //
 // If the local order of migrations does not match the order of migrations in
 // the schema history, an `ErrMismatchedMigration` error is returned.
@@ -22,46 +23,72 @@ func (m *Roll) UnappliedMigrations(ctx context.Context, dir fs.FS) ([]*migration
 		return nil, fmt.Errorf("determining latest version: %w", err)
 	}
 
-	files, err := migrations.CollectFilesFromDir(dir)
-	if err != nil {
-		return nil, fmt.Errorf("reading migration files: %w", err)
-	}
-
 	history, err := m.State().SchemaHistory(ctx, m.Schema())
 	if err != nil {
 		return nil, fmt.Errorf("reading schema history: %w", err)
 	}
 
-	// Find the index of the first unapplied migration
-	var idx int
-	if latestVersion != nil {
-		for _, file := range files {
-			migration, err := migrations.ReadRawMigration(dir, file)
-			if err != nil {
-				return nil, fmt.Errorf("reading migration file %q: %w", file, err)
-			}
-
-			remoteMigration := history[idx].Migration
-			if remoteMigration.Name != migration.Name {
-				return nil, fmt.Errorf("%w: remote=%q, local=%q", ErrMismatchedMigration, remoteMigration.Name, migration.Name)
-			}
-
-			idx++
-			if migration.Name == *latestVersion {
-				break
-			}
-		}
+	baseline, err := m.State().LatestBaseline(ctx, m.Schema())
+	if err != nil {
+		return nil, fmt.Errorf("reading baseline: %w", err)
 	}
 
-	// Return all unapplied migrations
-	migs := make([]*migrations.RawMigration, 0, len(files))
-	for _, file := range files[idx:] {
+	// Get all local migration files
+	files, err := migrations.CollectFilesFromDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("reading migration files: %w", err)
+	}
+
+	baselineName := ""
+	if baseline != nil {
+		baselineName = baseline.Name
+	}
+
+	// Find the index of the first local migration after the baseline
+	filesStartIdx := sort.Search(len(files), func(i int) bool {
+		var migration *migrations.RawMigration
+		migration, err = migrations.ReadRawMigration(dir, files[i])
+		if err != nil {
+			return false
+		}
+		return migration.Name > baselineName
+	})
+	if err != nil {
+		return nil, fmt.Errorf("finding migration after baseline: %w", err)
+	}
+
+	// Read all migrations that come after the baseline
+	migsAfterBaseline := make([]*migrations.RawMigration, 0, len(files))
+	for _, file := range files[filesStartIdx:] {
 		migration, err := migrations.ReadRawMigration(dir, file)
 		if err != nil {
 			return nil, fmt.Errorf("reading migration file %q: %w", file, err)
 		}
-		migs = append(migs, migration)
+		migsAfterBaseline = append(migsAfterBaseline, migration)
 	}
 
-	return migs, nil
+	// Find the index of the first local migration that has not been applied to
+	// the database and ensure that the order of migrations in the database
+	// matches the order of migrations in the local directory.
+	var appliedCount int
+	for _, m := range migsAfterBaseline {
+		// Stop when we've checked all the migrations in history
+		if appliedCount >= len(history) {
+			break
+		}
+
+		remoteMigration := history[appliedCount].Migration
+		if remoteMigration.Name != m.Name {
+			return nil, fmt.Errorf("%w: remote=%q, local=%q",
+				ErrMismatchedMigration, remoteMigration.Name, m.Name)
+		}
+
+		appliedCount++
+		if m.Name == *latestVersion {
+			break
+		}
+	}
+
+	// Return only the migrations that haven't been applied yet
+	return migsAfterBaseline[appliedCount:], nil
 }
